3832b4907cc8676525a5cf72f4a73d6041fc6b0b
~latter-bolden <latter-bolden@protonmail.com>
2022-07-19 13:54:08 -0400

    update have urbit dialog

--- app/src/renderer/system/onboarding/HaveUrbit.dialog.tsx
+++ app/src/renderer/system/onboarding/HaveUrbit.dialog.tsx
@@ -1,13 +1,23 @@
-import { FC, useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { createField, createForm } from 'mobx-easy-form';
+import { FC } from 'react';
+import styled, { css } from 'styled-components';
+import { fontSize, fontWeight, margin, MarginProps, FontSizeProps, FontWeightProps } from 'styled-system';
+// @ts-expect-error its there...
 import UrbitSVG from '../../../../assets/urbit.svg';
-import { Grid, Text, Flex, ActionButton } from 'renderer/components';
+import { Grid, Text, Flex, Button, TextButton } from 'renderer/components';
 import { observer } from 'mobx-react';
 import { BaseDialogProps } from 'renderer/system/dialog/dialogs';
 
-// some stuff goes here
+const SecondaryButtonText = styled.button<FontSizeProps & FontWeightProps & MarginProps>`
+  ${fontSize}
+  ${fontWeight}
+  ${margin}
+  color: rgba(101, 101, 101, 0.44);
+  border: none;
+  &:hover {
+    color: rgba(101, 101, 101, 0.65);
+  }
+`
+
 const HaveUrbitDialog: FC<BaseDialogProps> = observer(
   (props: BaseDialogProps) => {
     function selectSelfHosted() {
@@ -21,45 +31,46 @@ const HaveUrbitDialog: FC<BaseDialogProps> = observer(
     }
 
     return (
-      <Grid.Column noGutter lg={12} xl={12}>
-        <Flex flexDirection="row" alignItems="baseline" justifyContent="center">
-          <img style={{ height: '40px' }} src={UrbitSVG} alt="urbit logo" />
-          <Text
-            fontSize={3}
-            fontWeight={600}
-            mb={20}
-          >
-            Have an Urbit ID?
-          </Text>
-        </Flex>
-        <Text
-          fontSize={2}
-          fontWeight={300}
-          variant="body">
-            An Urbit ID is like a phone number. It’s how your friends connect with you on Urbit.
-            <br/><br/>
-            If you have one hosted already, you can connect it or get a new ID with hosting provided.
-        </Text>
-        <br/>
-        <Flex flexDirection="row" alignItems="baseline">
-          <ActionButton
-            tabIndex={-1}
-            height={36}
-            data-close-tray="true"
-            onClick={selectSelfHosted}
-          >
-            Connect
-          </ActionButton>
-          <span style={{ paddingLeft: "20px", paddingRight: "20px" }}>or</span>
-          <ActionButton
-            tabIndex={-1}
-            height={36}
-            data-close-tray="true"
-            onClick={selectHosted}
-          >
-            Get One
-          </ActionButton>
-        </Flex>
+      <Grid.Column noGutter lg={12} xl={12} px={32}>
+        <Grid.Row>
+           <Flex width="100%" height="100%" alignItems="center" justifyContent="center">
+            <img height={40} src={UrbitSVG} alt="urbit logo" />
+              <Text
+                ml={12}
+                fontSize={3}
+                fontWeight={400}
+              >
+                Have an Urbit ID?
+              </Text>
+           </Flex>
+        </Grid.Row>
+        <Grid.Row>
+          <Flex width="100%" height="100%" alignItems="center" justifyContent="center">
+            <Text
+              fontSize={2}
+              fontWeight={300}
+              lineHeight="copy"
+              variant="body">
+                An Urbit ID is like a phone number. It’s how your friends connect with you on Urbit.
+            </Text>
+          </Flex>
+        </Grid.Row>
+        <Grid.Row >
+          <Flex width="100%" height="100%" alignItems="center" justifyContent="center">
+            <Flex flexDirection="column" alignItems="top" justifyContent="space-between">
+              <Button
+                variant="minimal"
+                alignSelf="center"
+                fontWeight={500}
+                onClick={selectHosted}>
+                Sign up
+              </Button>
+              <SecondaryButtonText mt={20} fontSize={2} fontWeight={400} onClick={selectSelfHosted}>
+                Already have one?
+              </SecondaryButtonText>
+            </Flex>
+          </Flex>
+        </Grid.Row>
       </Grid.Column>
     )
   }
