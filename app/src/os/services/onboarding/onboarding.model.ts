90e377be3d5c525548f4f2cfe7b9cdfe33b37723
~latter-bolden <latter-bolden@protonmail.com>
2022-07-19 13:49:33 -0400

    update onboarding model and service

--- app/src/os/services/onboarding/onboarding.model.ts
+++ app/src/os/services/onboarding/onboarding.model.ts
@@ -1,21 +1,73 @@
-import { clear } from 'console';
 import {
-  detach,
   flow,
   Instance,
   types,
-  applySnapshot,
-  castToSnapshot,
 } from 'mobx-state-tree';
+import { LoaderModel } from '../common.model';
+import { HostingPlanet } from 'os/api/holium';
+
+export enum OnboardingStep {
+  DISCLAIMER = 'onboarding:disclaimer',
+  HAVE_URBIT_ID = 'onboarding:have-urbit-id',
+
+    ADD_SHIP = 'onboarding:add-ship',
+
+    SELECT_PATP = 'onboarding:hosted:select-patp',
+    SELECT_HOSTING_PLAN = 'onboarding:hosted:select-hosting-plan',
+    PAYMENT = 'onboarding:hosted:payment',
+    CONFIRMATION = 'onboarding:hosted:confirmation',
+
+  CONNECTING_SHIP = 'onboarding:connecting-ship',
+  PROFILE_SETUP = 'onboarding:profile-setup',
+  SET_PASSWORD = 'onboarding:set-password',
+  INSTALL_AGENT = 'onboarding:install-agent'
+};
+
+export const PlanetModel = types
+  .model({
+    patp: types.string,
+    sigil: types.string,
+    booted: types.boolean
+  })
+
+export const OnboardingShipModel = types
+  .model({
+    url: types.string,
+    id: types.identifier,
+    patp: types.string,
+    nickname: types.maybeNull(types.string),
+    color: types.maybeNull(types.string),
+    avatar: types.maybeNull(types.string),
+    cookie: types.maybeNull(types.string),
+  })
+  .actions((self) => ({
+    setContactMetadata: (contactMetadata: {
+      color?: string;
+      nickname?: string;
+      avatar?: string;
+    }) => {
+      self.color = contactMetadata.color || '#000000';
+      if (contactMetadata.nickname) self.nickname = contactMetadata.nickname;
+      if (contactMetadata.avatar) self.avatar = contactMetadata.avatar;
+    },
+  }))
 
 export const OnboardingStore = types
   .model({
+    currentStep: OnboardingStep.DISCLAIMER,
     agreedToDisclaimer: false,
-    selfHosted: false
+    selfHosted: false,
+    planet: types.maybe(PlanetModel),
+    ship: types.maybe(OnboardingShipModel),
+    installer: types.optional(LoaderModel, { state: 'initial' }),
   })
   .actions((self) => ({
 
-    agreedToDisclaimer() {
+    setStep(step: OnboardingStep) {
+      self.currentStep = step;
+    },
+
+    setAgreedToDisclaimer() {
       self.agreedToDisclaimer = true;
     },
 
@@ -23,9 +75,28 @@ export const OnboardingStore = types
       self.selfHosted = selfHosted;
     },
 
-    clear() {
-      applySnapshot(self, {})
-    }
-  }))
+    setPlanet(planet: HostingPlanet) {
+      self.planet = PlanetModel.create(planet)
+    },
 
-export type OnboardingStoreType = Instance<typeof OnboardingStore>
+    setShip: flow(function* (shipInfo: {
+      patp: string;
+      url: string;
+      cookie: string;
+    }) {
+      self.ship = OnboardingShipModel.create({ ...shipInfo, id: `onboarding${shipInfo.patp}` });
+    }),
+
+    installRealm: flow(function* () {
+      self.installer.set('loading');
+      self.installer.set('loaded');
+    }),
+
+    reset() {
+      self.ship = undefined;
+      self.agreedToDisclaimer = false;
+      self.currentStep = OnboardingStep.DISCLAIMER;
+    }
+  }));
+
+export type OnboardingStoreType = Instance<typeof OnboardingStore>;
--- app/src/os/services/onboarding/onboarding.service.ts
+++ app/src/os/services/onboarding/onboarding.service.ts
@@ -1,30 +1,42 @@
-import { ipcMain, session, ipcRenderer } from 'electron';
+import { ipcMain, ipcRenderer } from 'electron';
 import Store from 'electron-store';
 import {
+  clone,
   onPatch,
   onSnapshot,
   getSnapshot,
   castToSnapshot,
-  applySnapshot,
 } from 'mobx-state-tree';
 
 import Realm from '../..';
 import { BaseService } from '../base.service';
-import { OnboardingStore, OnboardingStoreType } from './onboarding.model';
+import { OnboardingStore, OnboardingStoreType, OnboardingStep } from './onboarding.model';
+import { AuthShip } from '../identity/auth.model';
+import { getCookie, ShipConnectionData } from '../../lib/shipHelpers';
+import { ContactApi } from '../../api/contacts';
+import { HostingPlanet } from 'os/api/holium';
 
 export class OnboardingService extends BaseService {
-  private db?: Store<OnboardingStoreType>; // for persistance
-  private state?: OnboardingStoreType; // for state management
+  private db: Store<OnboardingStoreType>; // for persistance
+  private state: OnboardingStoreType; // for state management
 
   handlers = {
+    'realm.onboarding.setStep': this.setStep,
     'realm.onboarding.agreedToDisclaimer': this.agreedToDisclaimer,
     'realm.onboarding.selfHosted': this.setSelfHosted,
-    'realm.onboarding.clear': this.clear
+    'realm.onboarding.getAvailablePlanets': this.getAvailablePlanets,
+    'realm.onboarding.addShip': this.addShip,
+    'realm.onboarding.selectPlanet': this.selectPlanet,
+    'realm.onboarding.getProfile': this.getProfile,
+    'realm.onboarding.setProfile': this.setProfile,
+    'realm.onboarding.setPassword': this.setPassword,
+    'realm.onboarding.installRealm': this.installRealm,
+    'realm.onboarding.completeOnboarding': this.completeOnboarding,
   }
 
   static preload = {
-    clear() {
-      return ipcRenderer.invoke('realm.onboarding.clear');
+    setStep(step: OnboardingStep) {
+      return ipcRenderer.invoke('realm.onboarding.setStep', step);
     },
 
     agreedToDisclaimer() {
@@ -33,26 +45,57 @@ export class OnboardingService extends BaseService {
 
     setSelfHosted(selfHosted: boolean) {
       return ipcRenderer.invoke('realm.onboarding.selfHosted', selfHosted);
+    },
+
+    getAvailablePlanets() {
+      return ipcRenderer.invoke('realm.onboarding.getAvailablePlanets');
+    },
+
+    addShip(shipInfo: ShipConnectionData) {
+      return ipcRenderer.invoke('realm.onboarding.addShip', shipInfo);
+    },
+
+    selectPlanet(patp: HostingPlanet) {
+      return ipcRenderer.invoke('realm.onboarding.selectPlanet', patp);
+    },
+
+    getProfile() {
+      return ipcRenderer.invoke('realm.onboarding.getProfile');
+    },
+
+    setProfile(profileData: {
+      nickname: string;
+      color: string;
+      avatar: string | null;
+    }) {
+      return ipcRenderer.invoke('realm.onboarding.setProfile', profileData);
+    },
+
+    setPassword(password: string) {
+      return ipcRenderer.invoke('realm.onboarding.setPassword', password);
+    },
+
+    installRealm() {
+      return ipcRenderer.invoke('realm.onboarding.installRealm');
+    },
+
+    completeOnboarding() {
+      return ipcRenderer.invoke('realm.onboarding.completeOnboarding');
     }
   }
 
   constructor(core: Realm, options: any = {}) {
     super(core, options);
 
-    Object.keys(this.handlers).forEach((handlerName: any) => {
-      // @ts-ignore
-      ipcMain.handle(handlerName, this.handlers[handlerName].bind(this));
-    });
-  }
-
-  async load(patp: string, mouseColor: string) {
     this.db = new Store({
-      name: `realm.onboarding.${patp}`,
+      name: `realm.onboarding`,
       accessPropertiesByDotNotation: true,
     });
 
     let persistedState: OnboardingStoreType = this.db.store;
-    this.state = OnboardingStore.create(castToSnapshot(persistedState));
+    this.state = Object.keys(persistedState).length !== 0
+      ? OnboardingStore.create(castToSnapshot(persistedState))
+      : OnboardingStore.create();
 
     onSnapshot(this.state, (snapshot) => {
       this.db!.store = castToSnapshot(snapshot);
@@ -61,22 +104,142 @@ export class OnboardingService extends BaseService {
     onPatch(this.state, (patch) => {
       const patchEffect = {
         patch,
-        resource: 'desktop',
+        resource: 'onboarding',
         response: 'patch',
       };
       this.core.onEffect(patchEffect);
     });
+
+    Object.keys(this.handlers).forEach((handlerName: any) => {
+      // @ts-ignore
+      ipcMain.handle(handlerName, this.handlers[handlerName].bind(this));
+    });
   }
 
-  clear() {
-    this.state?.clear();
+  get snapshot() {
+    return this.state ? getSnapshot(this.state) : null;
   }
 
-  agreedToDisclaimer() {
-    this.state?.agreedToDisclaimer();
+  reset() {
+    this.state.reset();
   }
 
-  setSelfHosted(selfHosted: boolean) {
-    this.state?.setSelfHosted(selfHosted);
+  setStep(_event: any, step: OnboardingStep) {
+    this.state.setStep(step);
+  }
+
+  agreedToDisclaimer(_event: any) {
+    this.state.setAgreedToDisclaimer();
+  }
+
+  setSelfHosted(_event: any, selfHosted: boolean) {
+    this.state.setSelfHosted(selfHosted);
+
+    if (!selfHosted) {
+      // pre-fetch available planets
+      this.core.holiumClient.getPlanets();
+    }
+  }
+
+  async getAvailablePlanets() {
+    return await this.core.holiumClient.getPlanets();
+  }
+
+  async addShip(
+    _event: any,
+    shipData: { patp: string; url: string; code: string }
+  ) {
+    try {
+      let { patp, url } = shipData;
+      let cookie = await getCookie(shipData);
+
+      this.state.setShip({
+        patp,
+        cookie,
+        url
+      });
+
+      return { url, cookie, patp };
+    } catch (reason) {
+      console.error('Failed to connect to ship', reason);
+      throw new Error('Failed to connect to ship')
+    }
+  }
+
+  async selectPlanet(_event: any, planet: HostingPlanet) {
+    this.state.setPlanet(planet);
+  }
+
+  async getProfile(_event: any) {
+    if (!this.state.ship)
+      throw new Error('Cannot get profile, onboarding ship not set.')
+
+    console.log('get profile', castToSnapshot(this.state.ship))
+
+    const { url, cookie, patp } = this.state.ship;
+    const ourProfile = await ContactApi.getContact(patp, {
+      ship: patp,
+      url,
+      cookie: cookie!,
+    });
+
+    this.state.ship.setContactMetadata(ourProfile);
+    return ourProfile;
+  }
+
+
+  async setProfile(
+    _event: any,
+    profileData: {
+      nickname: string;
+      color: string;
+      avatar: string | null;
+    }
+  ) {
+    console.log('setting profile', profileData)
+    if (!this.state.ship)
+      throw new Error('Cannot save profile, onboarding ship not set.');
+
+    const { url, cookie, patp } = this.state.ship!;
+    const credentials = {
+      ship: patp,
+      url,
+      cookie: cookie!
+    };
+
+    console.log('creds', credentials)
+
+    const updatedProfile = await ContactApi.saveContact(
+      patp,
+      credentials,
+      profileData
+    );
+
+    this.state.ship.setContactMetadata(updatedProfile);
+
+    return updatedProfile;
+  }
+
+  async setPassword(_event: any, password: string) {
+    // TODO store password hash
+    console.log('placeholder: saving password...')
+  }
+
+  async installRealm(_event: any, ship: string) {
+    // TODO kiln-install realm desk
+    console.log('placeholder: installing realm on ', ship);
+    this.state.installRealm();
+  }
+
+  async completeOnboarding(_event: any) {
+    if (!this.state.ship)
+      throw new Error('Cannot complete onboarding, ship not set.');
+
+    const ship = clone(this.state.ship);
+    const authShip = AuthShip.create({ ...ship, id: `auth${ship.patp}`});
+
+    this.core.services.identity.auth.storeNewShip(authShip);
+    this.core.services.identity.auth.setFirstTime();
+    this.core.services.ship.storeNewShip(authShip);
   }
 }
