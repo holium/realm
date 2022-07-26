27bc84d33a1f752e0d0bb22a473a6446f1d3b6c1
~latter-bolden <latter-bolden@protonmail.com>
2022-07-26 14:33:49 -0400

    update componenents

--- app/src/renderer/components/Input/Input.tsx
+++ app/src/renderer/components/Input/Input.tsx
@@ -50,7 +50,7 @@ export type InputProps = StyledComponentProps<
 
 export const InputWrapper = styled(Flex)`
   /* display: block; */
-  width: 100%;
+  width: ${(props) => props.width ? props.width : '100%'};
   pointer-events: none;
   transition: ${(props) => props.theme.transition};
   /* background-color: ${(props) => props.theme.colors.ui.tertiary}; */
@@ -239,6 +239,7 @@ const RightIcon: any = styled(Box)<
 export type FullProps = InputProps & {
   wrapperRef?: any;
   wrapperStyle?: any;
+  noCursor: boolean;
   rows?: any;
 };
 
@@ -257,12 +258,14 @@ export const Input: FC<FullProps> = forwardRef<HTMLInputElement, FullProps>(
       my,
       ml,
       mr,
+      noCursor,
       disabled,
       variant,
       borderColor,
       // eslint-disable-next-line @typescript-eslint/no-shadow
       color,
       bg,
+      width,
       wrapperRef,
       wrapperStyle,
     } = props;
@@ -280,6 +283,7 @@ export const Input: FC<FullProps> = forwardRef<HTMLInputElement, FullProps>(
         mt={mt}
         ml={ml}
         mr={mr}
+        width={width}
         flex={flex}
         style={wrapperStyle}
       >
@@ -302,7 +306,7 @@ export const Input: FC<FullProps> = forwardRef<HTMLInputElement, FullProps>(
           aria-invalid={props.error ? 'true' : 'false'}
           {...props}
           bg="transparent"
-          style={{ width: '100%' }}
+          style={{ width: '100%', caretColor: noCursor ? 'transparent' : 'inherit' }}
         />
         {rightIcon && (
           <RightIcon
