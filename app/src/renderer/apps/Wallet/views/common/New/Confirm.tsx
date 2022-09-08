98f781c29f6c90c81d78783b6cef179cd0046e57
~latter-bolden <latter-bolden@protonmail.com>
2022-09-08 16:51:53 -0500

    new wallet: confirm

---
+++ app/src/renderer/apps/Wallet/views/common/New/Confirm.tsx
@@ -0,0 +1,49 @@
+import { FC, useEffect, useState, Dispatch, SetStateAction } from 'react';
+import { observer } from 'mobx-react';
+import { Button, Flex, Text } from 'renderer/components';
+import { darken, transparentize } from 'polished';
+import { useTrayApps } from 'renderer/logic/apps/store';
+import { useServices } from 'renderer/logic/store';
+import { WordPicker } from './WordPicker';
+import { NewWalletScreen } from './index';
+
+interface ConfirmProps {
+  seedPhrase: string
+  setScreen: Dispatch<SetStateAction<NewWalletScreen>>
+}
+
+export const Confirm: FC<ConfirmProps> = observer((props: ConfirmProps) => {
+  let { setTrayAppDimensions, dimensions } = useTrayApps();
+  let [valid, setValid] = useState(false);
+  const { desktop } = useServices();
+
+  const panelBackground = darken(0.02, desktop.theme!.windowColor);
+  const panelBorder = `2px solid ${transparentize(0.9, '#000000')}`;
+
+  useEffect(() => {
+    let prevDims = dimensions;
+    console.log(prevDims.height, prevDims.width)
+    setTrayAppDimensions({ height: 560, width: 360 });
+
+    return () => {
+      setTrayAppDimensions(prevDims);
+    }
+  }, [])
+
+  return (
+    <Flex width="100%" height="100%" flexDirection="column" justifyContent="space-evenly" alignItems="center">
+      <Flex flexDirection="column">
+        <Text variant="h5">Confirm words</Text>
+        <Text mt={3} variant="body">
+          Verify you wrote the secret recovery phrase down correctly by clicking the following words in the correct order.
+        </Text>
+      </Flex>
+      <Flex flexDirection="column" alignItems="center">
+        <WordPicker seedPhrase={props.seedPhrase} border={panelBorder} background={panelBackground} onValidChange={setValid} />
+      </Flex>
+      <Flex justifyContent="center" alignItems="center">
+        <Button disabled={!valid} onClick={() => props.setScreen(NewWalletScreen.PASSCODE)}>Confirm</Button>
+      </Flex>
+    </Flex>
+  )
+});
