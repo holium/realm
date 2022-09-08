90aab8e44222445fb94a0399a7575e4233177321
~latter-bolden <latter-bolden@protonmail.com>
2022-09-08 17:34:04 -0500

    fix conflicts wallet index

--- app/src/renderer/apps/Wallet/index.tsx
+++ app/src/renderer/apps/Wallet/index.tsx
@@ -1,10 +1,13 @@
 import { observer } from 'mobx-react';
-import { FC, useEffect } from 'react';
-import { useTrayApps } from 'renderer/apps/store';
+import { ThemeProvider } from 'styled-components';
+import { FC, useEffect, useState } from 'react';
+import { useTrayApps } from 'renderer/logic/apps/store';
 import { WalletSettings } from './views/common/Settings';
 import { EthDetail } from './views/ethereum/Detail';
 import { EthList } from './views/ethereum/List';
 import { EthTransaction } from './views/ethereum/Transaction';
+import { EthNew } from './views/common/New';
+import { WalletNetwork } from './views/common/Network';
 
 import { WalletHeader } from './views/common/Header';
 import { useServices } from 'renderer/logic/store';
@@ -19,6 +22,7 @@ export const WalletViews: { [key: string]: any } = {
   'ethereum:list': (props: any) => <EthList {...props} />,
   'ethereum:detail': (props: any) => <EthDetail {...props} />,
   'ethereum:transaction': (props: any) => <EthTransaction {...props} />,
+  'ethereum:new': (props: any) => <EthNew {...props} />,
   settings: (props: any) => <WalletSettings {...props} />,
 };
 
@@ -31,16 +35,18 @@ export const WalletApp: FC<any> = observer((props: any) => {
 
   const View = WalletViews[walletApp.currentView];
   return (
-    <Flex flexDirection="column">
-      <WalletHeader
-        theme={desktop.theme}
-        network={walletApp.network}
-        onAddWallet={onAddWallet}
-        onSetNetwork={(network: any) => {
-          walletApp.setNetwork(network);
-        }}
-      />
-      <View {...props} />
-    </Flex>
+      <Flex position="relative" height="100%" width="100%" flexDirection="column">
+        <WalletHeader
+          theme={desktop.theme}
+          network={walletApp.network}
+          onAddWallet={onAddWallet}
+          onSetNetwork={(network: any) => {
+            walletApp.setNetwork(network);
+          }}
+          hide={walletApp.currentView === 'ethereum:new'}
+        />
+        <View {...props} />
+        <WalletNetwork hidden={walletApp.currentView === 'ethereum:new'} theme={desktop.theme} />
+      </Flex>
   );
 });
