e0c44bb183517ae05bfa1b6966b68d80d9e930bc
bacwyls <bacwyls@gmail.com>
2022-09-12 11:23:24 -0500

    rooms: improve the info view, fixing a bug with wrapping text in the table

--- app/src/renderer/apps/Rooms/Room/Info.tsx
+++ app/src/renderer/apps/Rooms/Room/Info.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import { useTrayApps } from 'renderer/apps/store';
 import { Flex, Grid, Icons, Text } from 'renderer/components';
 // import Row from 'renderer/components/Row';
+import styled from 'styled-components';
 import { Row } from 'renderer/components/NewRow';
 import { useServices } from 'renderer/logic/store';
 
@@ -17,66 +18,78 @@ export const RoomInfo: FC<RoomInfoProps> = (props: RoomInfoProps) => {
 
   const rowGap = 16
   
+  const swid = '75px'
+  const bwid = '175px'
+
+
+const Key = styled(Text)`
+  width:${swid};
+  max-width:${swid};
+  min-width:${swid};
+`;
+
+const Value = styled(Text)`
+  width:${bwid};
+  max-width:${bwid};
+  min-width:${bwid};
+`;
 
   return (
     
-    <Flex flex={2} gap={16} p={2} flexDirection="column" alignItems="center">
-      <Flex flexDirection='row'
-        overflowX="scroll"
-        opacity={0.7}
-        style={{
-          cursor: 'none',
-          borderRadius: 6,
-          // backgroundColor: theme.inputColor,
-          // backgroundColor: 'rgba(0, 0, 0, 0.05)',
-          // color: 'rgba(0, 0, 0, 0.5)',
-          padding: 12,
-          paddingBottom: 16,
-          paddingTop: 16,
-          maxHeight:300,
-          maxWidth:300,
-          // TODO border?
-          // borderColor: theme.textColor,
-          // borderStyle: 'solid',
-          // border: 1
-        }}
-        >
-        <Grid.Column gap={rowGap}>
-          {/* TODO there are UI problems here that are out of my league.
-            tabular stuff hasnt been fully thought through yet.
-            specifically there are nuances with wrapping long strings (room id and moon/comet @ps)
-            and maintaining the grids vertical + horizontal alignment. temp solution is just to scroll,
-            but thats pretty ugly.
-            ~bacwyl-samweg
-             */}
-          <Text>Room ID:</Text>
-          <Text>Creator:</Text>
+    <Flex flex={2} p={2} mt={6}
+      flexDirection="column"
+      alignItems="center"
+      opacity={0.7}
+      style={{
+        overflowWrap: 'break-word'
+      }}
+      >
 
-          <Text>Access:</Text>
-          <Text>Capacity:</Text>
-          {/* <Text>Whitelist:</Text> */}
+        <Flex flexDirection='row' gap={16} width={'80%'} pb={16}>
+            <Key>
+            Creator:
+            </Key>
+            <Value>
+            {/* {room.creator} */}
+            ~sampel-palnet-sampel-palnet  
+            </Value>
+        </Flex>
 
-        </Grid.Column>
+        <Flex flexDirection='row' gap={16} width={'80%'} pb={16}>
+            <Key>
+            Access:
+            </Key>
+            <Value>
+            {room.access}
+            </Value>
+        </Flex>
 
-        <Grid.Column gap={rowGap}>
-          <Text>{room.id}</Text>
-          <Text>{room.creator}</Text>
+        <Flex flexDirection='row' gap={16} width={'80%'} pb={16}>
+           <Key>
+            Capacity:
+            </Key>
+            <Value>
+            {`${room.present.length}/${room.capacity}`}
+            </Value>
+        </Flex>
 
-          <Text>{room.access}</Text>
-
-          <Text>{`${room.present.length}/${room.capacity}`}</Text>
-
-          {/* <Flex flexDirection='column'>
-          {room.whitelist.map((patp: Patp, index: number) => {
-            return (
-              <Flex key={`room-whitelist-${patp}`}>
-                <Text>{patp}</Text>
-              </Flex>
-              )})}
-          </Flex> */}
-        </Grid.Column>
-      </Flex>
-       
+        <Flex flexDirection='row' gap={16} width={'80%'} pb={16}>
+             <Key>
+            Whitelist:
+            </Key>
+            <Flex width={bwid} maxWidth={bwid} minWidth={bwid}
+              flexDirection={'column'}
+              maxHeight={'120px'}
+              overflowY={'scroll'}>
+              {room.whitelist.map((patp: Patp, index: number) => {
+                return (
+                  <Flex key={`room-whitelist-${patp}`} mb={2}>
+                    <Text>{patp}</Text>
+                  </Flex>
+                  )})}
+          </Flex>
+        </Flex>
+          
     </Flex>
   );
 };
