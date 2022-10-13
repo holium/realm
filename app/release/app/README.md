71497d3d52c801826903a255fd50e5aedd9a6103
~lodlev-migdev <patrick@holium.com>
2022-10-13 11:24:48 -0500

    version bump. started readme. more update code cleanup to hide certain messages when auto update

---
+++ app/release/app/README.md
@@ -0,0 +1,17 @@
+# Production Release Notes
+
+## End Usage
+
+### Auto Updates
+
+Realm checks for updates when the app starts. Checks for updates will then occur every 10 mins after the initial check.
+
+If updates are available, you will be prompted to download and install the updates.
+
+### Manual Update
+
+Manual checks can also be performed by clicking the Realm -> Check for Updates menu.
+
+If updates are available, you will be prompted to download and install the updates.
+
+## Technical Notes
--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -18,7 +18,7 @@ import {
   dialog,
   MessageBoxReturnValue,
 } from 'electron';
-import { autoUpdater } from 'electron-updater';
+import { autoUpdater, UpdateCheckResult } from 'electron-updater';
 import log from 'electron-log';
 import isDev from 'electron-is-dev';
 import MenuBuilder from './menu';
@@ -47,6 +47,7 @@ export interface IAppUpdater {
 }
 
 export class AppUpdater implements IAppUpdater {
+  private manualCheck: boolean = false;
   constructor() {
     autoUpdater.autoInstallOnAppQuit = true;
     autoUpdater.autoDownload = true;
@@ -79,10 +80,13 @@ export class AppUpdater implements IAppUpdater {
         });
     });
     autoUpdater.on('update-not-available', () => {
-      dialog.showMessageBox({
-        title: 'No Updates',
-        message: 'Current version is up-to-date.',
-      });
+      // only show this message if the user chose to run an update check manually
+      if (this.manualCheck) {
+        dialog.showMessageBox({
+          title: 'No Updates',
+          message: 'Current version is up-to-date.',
+        });
+      }
     });
     autoUpdater.on('update-downloaded', () => {
       dialog
@@ -97,15 +101,16 @@ export class AppUpdater implements IAppUpdater {
     log.transports.file.level = 'debug';
     autoUpdater.logger = log;
     // run auto check once every 10 minutes after app starts
-    setInterval(() => autoUpdater.checkForUpdates(), 600000);
+    setInterval(() => {
+      if (!this.manualCheck) {
+        autoUpdater.checkForUpdates();
+      }
+    }, 600000);
     autoUpdater.checkForUpdates();
   }
   checkForUpdates = () => {
-    dialog.showMessageBox({
-      title: 'Build',
-      message: `${process.env.NODE_ENV}`,
-    });
-    autoUpdater.checkForUpdates();
+    this.manualCheck = true;
+    autoUpdater.checkForUpdates().finally(() => (this.manualCheck = false));
   };
 }
 
