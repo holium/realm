94de819d411a992d05d428f10396259831268113
~lodlev-migdev <patrick@holium.com>
2022-10-20 09:21:05 -0500

    testing auto-version increment

--- .github/workflows/scripts/verinc.js
+++ .github/workflows/scripts/verinc.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
 module.exports = ({ github, context }) => {
   const pkg = JSON.parse(fs.readFileSync("./app/release/app/package.json"));
-  return pkg.value;
+  return pkg.version;
 };
