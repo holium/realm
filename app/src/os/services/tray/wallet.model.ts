87ef9be3647e867a837aee89a6583b0f86f8da61
Leo Winiecki <leo@holium.com>
2022-11-04 12:51:35 -0700

    fixed the network on senderc20

--- app/src/os/services/tray/wallet.model.ts
+++ app/src/os/services/tray/wallet.model.ts
@@ -397,7 +397,7 @@ const EthWallet = types
         [hash]: tx
       }
       self.transactions.set(network, getSnapshot(types.map(EthTransaction).create(newMap)));
-      console.log(self.transactions.get(self.network)!.get(hash))
+      console.log(self.transactions.get(network)!.get(hash))
     },
     applyTransactionUpdate(network: string, transaction: any) {
       let netMap = self.transactions.get(network)
