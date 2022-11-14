a4d6a7a61b823e7e298525b07d805eac8e097786
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2022-11-14 13:56:29 -0600

    working on a config file for dev apps

--- app/src/renderer/system/desktop/components/Home/Ship/AppGrid.tsx
+++ app/src/renderer/system/desktop/components/Home/Ship/AppGrid.tsx
@@ -2,11 +2,16 @@ import { FC, useMemo } from 'react';
 import { toJS } from 'mobx';
 import { observer } from 'mobx-react';
 import { AppTile, AppTileSize } from 'renderer/components/AppTile';
-import { AppType, InstallStatus } from 'os/services/spaces/models/bazaar';
+import {
+  AppType,
+  AppTypes,
+  InstallStatus,
+} from 'os/services/spaces/models/bazaar';
 import { useServices } from 'renderer/logic/store';
 import { DesktopActions } from 'renderer/logic/actions/desktop';
 import { SpacesActions } from 'renderer/logic/actions/spaces';
 import { ShellActions } from 'renderer/logic/actions/shell';
+import { devApps } from 'renderer/apps/development';
 
 type AppGridProps = {
   isOpen?: boolean;
@@ -15,30 +20,29 @@ type AppGridProps = {
 
 export const AppGrid: FC<AppGridProps> = observer((props: AppGridProps) => {
   const { isOpen, tileSize } = props;
-  const { spaces, bazaar, ship } = useServices();
-
+  const { spaces, bazaar } = useServices();
   const currentSpace = spaces.selected!;
   const dock = bazaar.getDock(currentSpace.path);
 
-  // const currentBazaar = useMemo(() => bazaar.getPinnedApps(currentSpace.path), [currentSpace]);
-  // const currentBazaar = bazaar.getBazaar(currentSpace.path);
-
   return useMemo(() => {
     const apps = [...bazaar.installed, ...bazaar.installing];
-    // apps.push(bazaar.installing);
+
     return (
       <>
         {apps.map((app: any, index: number) => {
           const isAppPinned = bazaar.isPinned(currentSpace.path, app.id);
           const weRecommended = bazaar.isRecommended(app.id);
+          let isInstalling = app.installStatus !== InstallStatus.installed;
+          if (app.type === AppTypes.Web) {
+            isInstalling = false;
+          }
           return (
             <AppTile
               key={app.title + index + 'grid'}
               isPinned={isAppPinned}
               isRecommended={weRecommended}
               allowContextMenu
-              // isInstalling
-              isInstalling={app.installStatus !== InstallStatus.installed}
+              isInstalling={isInstalling}
               tileSize={tileSize}
               app={app}
               isVisible={isOpen}
@@ -73,7 +77,6 @@ export const AppGrid: FC<AppGridProps> = observer((props: AppGridProps) => {
                     );
                   },
                 },
-
                 ...(app.type === 'urbit' &&
                 app.installStatus === InstallStatus.installed
                   ? [
@@ -96,7 +99,6 @@ export const AppGrid: FC<AppGridProps> = observer((props: AppGridProps) => {
                         onClick: (evt: any) => {
                           evt.stopPropagation();
                           // console.log(`start install`, toJS(app));
-
                           // SpacesActions.installApp(toJS(app));
                         },
                       },
