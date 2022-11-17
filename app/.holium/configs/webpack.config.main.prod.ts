201fa743753a31f4c236636da2a45b923cb33e19
lomder-librun <6413077+drunkplato@users.noreply.github.com>
2022-11-17 11:11:18 -0600

    Update webpack.config.main.prod.ts
    changing `wallet` to `realm-wallet`

--- app/.holium/configs/webpack.config.main.prod.ts
+++ app/.holium/configs/webpack.config.main.prod.ts
@@ -59,7 +59,7 @@ const configuration: webpack.Configuration = {
       DEBUG_PROD: false,
       START_MINIMIZED: false,
       GH_TOKEN: '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',
-      INSTALL_MOON: '~hostyv:realm,courier,wallet',
+      INSTALL_MOON: '~hostyv:realm,courier,realm-wallet',
     }),
   ],
   /**
