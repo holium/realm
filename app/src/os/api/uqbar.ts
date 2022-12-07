5ba577ea3648da584255fe8f190c62a26469d296
Leo Winiecki <leodwiniecki@outlook.com>
2022-12-07 14:48:27 -0600

    threw in the uqbar pokes

--- app/src/os/api/uqbar.ts
+++ app/src/os/api/uqbar.ts
@@ -15,6 +15,83 @@ export const WalletApi = {
     };
     await conduit.poke(payload);
   },
+  editNickname: async (conduit: Conduit, address: string, nick: string) => {
+    const payload = {
+      app: 'wallet',
+      mark: 'wallet-poke',
+      json: {
+        'edit-nickname': {
+          address,
+          nick
+        }
+      }
+    }
+    await conduit.poke(payload);
+  },
+  deleteAccount: async (conduit: Conduit, address: string) => {
+    const payload = {
+      app: 'wallet',
+      mark: 'wallet-poke',
+      json: {
+        'delete-address': {
+          address
+        }
+      }
+    };
+    conduit.poke(payload);
+  },
+  sendTokens: async (conduit: Conduit, from: string, contract: string, town: string, to: string, item: string, amount: number) => {
+    const payload = {
+      app: 'wallet',
+      mark: 'wallet-poke',
+      json: {
+        transaction: {
+          from,
+          contract,
+          town,
+          action: {
+            give: {
+              to,
+              amount,
+              item
+            }
+          }
+        }
+      }
+    }
+    await conduit.poke(payload);
+  },
+  getTransactions: async (conduit: Conduit) => {
+    return conduit.scry({
+      app: 'wallet',
+      path: `/transactions`
+    })
+  },
+  setNode: async (conduit: Conduit, town: number, ship: string) => {
+    const payload = {
+      app: 'wallet',
+      mark: 'wallet-poke',
+      json: {
+        'set-node': {
+          town,
+          ship
+        }
+      }
+    }
+    await conduit.poke(payload);
+  },
+  setIndexer: async (conduit: Conduit, ship: string) => {
+    const payload = {
+      app: 'wallet',
+      mark: 'wallet-poke',
+      json: {
+        'set-indexer': {
+          ship
+        }
+      }
+    }
+    await conduit.poke(payload);
+  },
   /**
    * watchUpdates
    *
@@ -26,15 +103,37 @@ export const WalletApi = {
       walletState: WalletStoreType,
     ): void => {
       conduit.watch({
-        app: 'realm-wallet',
-        path: '/updates',
+        app: 'wallet',
+        path: '/book-updates',
         onEvent: async (data: any, _id?: number, mark?: string) => {
-          if (mark === 'realm-wallet-update') {
-            handleWalletReactions(
-              data,
-              walletState,
-            );
-          }
+          handleBookReactions(
+            data,
+            walletState,
+          );
+        },
+        onError: () => console.log('Subscription rejected'),
+        onQuit: () => console.log('Kicked from subscription %spaces'),
+      });
+      conduit.watch({
+        app: 'wallet',
+        path: '/metadata-updates',
+        onEvent: async (data: any, _id?: number, mark?: string) => {
+          handleMetadataReactions(
+            data,
+            walletState,
+          );
+        },
+        onError: () => console.log('Subscription rejected'),
+        onQuit: () => console.log('Kicked from subscription %spaces'),
+      });
+      conduit.watch({
+        app: 'wallet',
+        path: '/tx-updates',
+        onEvent: async (data: any, _id?: number, mark?: string) => {
+          handleTxReactions(
+            data,
+            walletState,
+          );
         },
         onError: () => console.log('Subscription rejected'),
         onQuit: () => console.log('Kicked from subscription %spaces'),
@@ -42,63 +141,20 @@ export const WalletApi = {
     },
 };
 
-export const handleWalletReactions = (
+const handleBookReactions = (
   data: any,
   walletState: WalletStoreType,
 ) => {
-  const reaction: string = Object.keys(data)[0];
-  switch (reaction) {
-    case 'wallet':
-      console.log('got wallet update')
-      const wallet = data.wallet;
-      console.log(wallet);
-      if (wallet.network === 'ethereum') {
-        walletState!.ethereum.applyWalletUpdate(
-          walletState!.navState.protocol,
-          wallet
-        );
-      } else if (wallet.network === 'bitcoin') {
-        walletState!.bitcoin.applyWalletUpdate(wallet);
-      } else if (wallet.network === 'btctestnet') {
-        walletState!.btctest.applyWalletUpdate(wallet);
-      }
-      break;
-    case 'wallets':
-      console.log('got wallets update')
-      const wallets = data.wallets;
-      if (
-        Object.keys(wallets.ethereum).length !== 0 ||
-        Object.keys(wallets.bitcoin).length !== 0 ||
-        Object.keys(wallets.btctestnet).length !== 0
-      ) {
-        walletState!.setInitialized(true);
-      }
-      walletState!.ethereum.initial(wallets);
-      walletState!.bitcoin.initial(wallets.bitcoin);
-      walletState!.btctest.initial(wallets.btctestnet);
-      break;
-    case 'transaction':
-      const transaction = data.transaction;
-      if (transaction.network == 'ethereum')
-        walletState!.ethereum.wallets.get(
-          transaction.index
-        )!.applyTransactions(transaction.net, transaction.transaction);
-      else if (transaction.network == 'bitcoin') {
-        walletState!.ethereum.wallets.get(
-          transaction.index
-        )!.applyTransactions(transaction.net, transaction.transaction);
-        // walletState!.wallets.get(NetworkStoreType.BTC_MAIN)!.wallets.get(transaction.index)!.applyTransactionUpdate(transaction);
-      }
-      else if (transaction.network == 'btctest') {
-        walletState!.ethereum.wallets.get(
-          transaction.index
-        )!.applyTransactions(transaction.net, transaction.transaction);
-      }
-      break;
-    case 'settings':
-      walletState.setSettings(data);
-      break;
-    default:
-      break;
-  }
+};
+
+const handleMetadataReactions = (
+  data: any,
+  walletState: WalletStoreType,
+) => {
+};
+
+const handleTxReactions = (
+  data: any,
+  walletState: WalletStoreType,
+) => {
 };
--- app/src/os/services/tray/wallet/protocols/ethereum.ts
+++ app/src/os/services/tray/wallet/protocols/ethereum.ts
@@ -2,15 +2,14 @@ import {
   BaseProtocol,
 } from '@holium/realm-wallet/src/wallets/BaseProtocol';
 import { BaseAsset } from '@holium/realm-wallet/src/wallets/BaseAsset';
-import { Alchemy, Network } from 'alchemy-sdk';
+import { Alchemy, AssetTransfersCategory, Network } from 'alchemy-sdk';
 import axios from 'axios';
-import { ethers } from 'ethers';
+import { ethers, utils } from 'ethers';
 // @ts-expect-error
 import abi from 'human-standard-token-abi';
 // @ts-expect-error
 import nftabi from 'non-fungible-token-abi';
-import { ProtocolType, WalletStoreType, Asset } from '@holium/realm-wallet/src/wallet.model';
-import { parseEther } from 'ethers/lib/utils';
+import { ProtocolType, WalletStoreType, Asset, CoinAsset, NFTAsset } from '@holium/realm-wallet/src/wallet.model';
 
 export class EthereumProtocol implements BaseProtocol {
   private network: ProtocolType;
@@ -66,12 +65,12 @@ export class EthereumProtocol implements BaseProtocol {
         .then((response: any) => {
           ethWallet.applyTransactions(this.network, response.data.result)
         })
-      this.getAccountAssets(ethWallet.address).then();
+      this.getAccountAssets(ethWallet.address).then(console.log);
     }
   }
 
   async getAccountBalance(addr: string): Promise<string> {
-    return (await this.ethProvider!.getBalance(addr)).toString();
+    return utils.formatEther(await this.ethProvider!.getBalance(addr));
   }
   async getAccountTransactions(addr: string, startBlock: number): Promise<any[]> {
     const apiKey = 'EMD9R77ARFM6AYV2NMBTUQX4I5TM5W169G';
@@ -80,11 +79,47 @@ export class EthereumProtocol implements BaseProtocol {
     const URL = this.network === ProtocolType.ETH_MAIN ? mainnetURL : goerliURL;
     return await axios.get(URL);
   }
-  async getAccountAssets(addr: string): Promise<Asset[] | undefined> {
-    console.log('getting account assets')
-    const metadata = await this.alchemy.core.getTokenMetadata(addr)
-    console.log(metadata);
-    return undefined;
+  async getAccountAssets(addr: string): Promise<Asset[]> {
+    const coins = await this.alchemy.core.getTokenBalances(addr);
+    const nfts = await this.alchemy.nft.getNftsForOwner(addr);
+    let assets: Asset[] = [];
+    for (let coin of coins.tokenBalances) {
+      console.log('getting account assets')
+      const metadata = await this.alchemy.core.getTokenMetadata(coin.contractAddress);
+      console.log(metadata);
+      console.log(metadata);
+      const data: CoinAsset = {
+        logo: metadata.logo,
+        symbol: metadata.symbol || '',
+        decimals: metadata.decimals || 0,
+        balance: 1, // coin.tokenBalance?,
+        totalSupply: 1,
+        allowances: {},
+      }
+      const asset: Asset = {
+        addr: coin.contractAddress,
+        type: 'coin',
+        data
+      }
+      assets.push(asset)
+    }
+    for (let nft of nfts.ownedNfts) {
+      const data: NFTAsset = {
+        name: nft.title,
+        description: nft.description,
+        image: nft.rawMetadata?.image || '',
+        transferable: true,
+        properties: {},
+      }
+      const asset: Asset = {
+        addr: nft.contract.address,
+        id: nft.tokenId,
+        type: 'nft',
+        data
+      }
+      assets.push(asset);
+    }
+    return assets
   }
   async sendTransaction(signedTx: string): Promise<any> {
     return (await this.ethProvider!.sendTransaction(signedTx)).hash;
@@ -96,8 +131,12 @@ export class EthereumProtocol implements BaseProtocol {
     const ethContract = new ethers.Contract(contract, abi, this.ethProvider);
     return (await ethContract.balanceOf(addr)).toString();
   }
-  getAssetMetadata(contract: string, addr: string): Promise<Asset> {
-    throw new Error('Method not implemented.');
+  async getAssetMetadata(contract: string): Promise<Asset> {
+    console.log('getting account assets')
+    const metadata = await this.alchemy.core.getTokenMetadata(contract)
+    console.log(metadata);
+    let asset: Asset;
+    return asset;
   }
   getAssetAllowance(contract: string, addr: string): Promise<number> {
     const ethContract = new ethers.Contract(contract, abi, this.ethProvider!);
@@ -126,8 +165,8 @@ export class EthereumProtocol implements BaseProtocol {
     );*/
     return (await ethContract.transfer(toAddr, amountOrTokenId)).hash;
   }
-  getFeePrice(): number | Promise<number> {
-    return 0;
+  async getFeePrice(): Promise<number> {
+    return Number((await this.alchemy.core.getFeeData()).gasPrice?._hex.toString());
   }
   async getFeeEstimate(
     from: string,
