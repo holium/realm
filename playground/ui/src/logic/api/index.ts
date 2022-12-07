00c1167c0e05a7efe3efb7f854a101a12619fb68
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2022-12-07 07:28:33 -0600

    moved playground to another repo for archiving

--- playground/ui/src/logic/api/index.ts
+++
@@ -1,8 +0,0 @@
-import Urbit from '@urbit/http-api';
-const api = new Urbit('', '', (window as any).desk);
-api.ship = window.ship;
-api.verbose = true;
-// @ts-expect-error TODO window typings
-window.api = api;
-
-export default api;
