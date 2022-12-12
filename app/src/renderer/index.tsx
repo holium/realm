9b7290b999605eafc669a57b2d255663dd0f0a3b
gdbroman <gustaf.broman@tink.com>
2022-12-12 10:28:46 -0600

    Add Amplitude

--- app/src/renderer/index.tsx
+++ app/src/renderer/index.tsx
@@ -1,12 +1,27 @@
 import { createRoot } from 'react-dom/client';
 import * as Sentry from '@sentry/react';
+import * as Amplitude from '@amplitude/analytics-browser';
 import { BrowserTracing } from '@sentry/tracing';
 import App from './App';
 
+const environment = process.env.NODE_ENV;
+const isProd = environment === 'production';
+const isDebug = process.env.DEBUG_PROD === 'true';
+
 Sentry.init({
+  debug: isDebug,
+  enabled: isProd,
+  environment,
   dsn: 'https://56fbf5e600db48cf8a785931be1ca5e4@o1327359.ingest.sentry.io/4504310987358208',
   integrations: [new BrowserTracing()],
-  tracesSampleRate: process.env.NODE_ENV === 'production' ? 1 : 0,
+  tracesSampleRate: 1,
+});
+
+Amplitude.init('d6d123a2a660806abcc6b1845c475f2f', undefined, {
+  optOut: !isProd,
+  trackingOptions: {
+    ipAddress: false,
+  },
 });
 
 const container = document.getElementById('root')!;
