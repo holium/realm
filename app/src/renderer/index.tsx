7db03f57cb274fc25ed6335e8698d6e592139ad2
gdbroman <gustaf.broman@tink.com>
2022-12-12 11:23:37 -0600

    extract api keys

--- app/src/renderer/index.tsx
+++ app/src/renderer/index.tsx
@@ -8,21 +8,30 @@ const environment = process.env.NODE_ENV;
 const isProd = environment === 'production';
 const isDebug = process.env.DEBUG_PROD === 'true';
 
-Sentry.init({
-  debug: isDebug,
-  enabled: isProd,
-  environment,
-  dsn: 'https://56fbf5e600db48cf8a785931be1ca5e4@o1327359.ingest.sentry.io/4504310987358208',
-  integrations: [new BrowserTracing()],
-  tracesSampleRate: 1,
-});
-
-Amplitude.init('d6d123a2a660806abcc6b1845c475f2f', undefined, {
-  optOut: !isProd,
-  trackingOptions: {
-    ipAddress: false,
-  },
-});
+const sentryDsn = process.env.SENTRY_DSN;
+if (sentryDsn) {
+  Sentry.init({
+    enabled: isProd,
+    debug: isDebug,
+    environment,
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1,
+  });
+} else {
+  console.error('Environment variable SENTRY_DSN is not defined.');
+}
+const amplitudeApiKey = process.env.AMPLITUDE_API_KEY;
+if (amplitudeApiKey) {
+  Amplitude.init(amplitudeApiKey, undefined, {
+    optOut: !isProd,
+    trackingOptions: {
+      ipAddress: false,
+    },
+  });
+} else {
+  console.error('Environment variable AMPLITUDE_API_KEY is not defined.');
+}
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
