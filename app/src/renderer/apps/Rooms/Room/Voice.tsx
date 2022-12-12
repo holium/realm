a21cc2cfef9bb99a2b5b6317bcff6fb665e9ea75
gdbroman <gustaf.broman@tink.com>
2022-12-12 12:01:52 -0600

    show correct host

--- app/src/renderer/apps/Rooms/Room/Voice.tsx
+++ app/src/renderer/apps/Rooms/Room/Voice.tsx
@@ -1,35 +1,37 @@
 import { observer } from 'mobx-react';
-import { FC } from 'react';
 import { Flex } from 'renderer/components';
 import { Speaker } from '../components/Speaker';
 import { useRooms } from '../useRooms';
 
-interface VoiceViewProps {
-  host: string;
-}
-
-export const VoiceView: FC<VoiceViewProps> = observer(
-  ({ host }: VoiceViewProps) => {
-    const roomsManager = useRooms();
-    if (!roomsManager.presentRoom) {
-      return null;
-    }
-    const peers = Array.from(roomsManager.protocol.peers.keys());
-    return (
-      <Flex
-        flex={2}
-        gap={12}
-        py={2}
-        display="grid"
-        gridTemplateColumns={peers.length + 1 ? `repeat(2, 1fr)` : '.5fr'}
-        gridAutoColumns="1fr"
-        gridAutoRows={'.5fr'}
-      >
-        <Speaker type="host" person={host} />
-        {peers.map((person: string) => (
-          <Speaker key={person} type="speaker" person={person} />
-        ))}
-      </Flex>
-    );
+export const VoiceView = observer(() => {
+  const roomsManager = useRooms();
+  if (!roomsManager.presentRoom) {
+    return null;
   }
-);
+
+  const us = roomsManager.protocol.our;
+  const host = roomsManager.protocol.provider;
+  const remainingPeers = Array.from(roomsManager.protocol.peers.keys()).filter(
+    (patp) => patp !== host
+  );
+
+  return (
+    <Flex
+      flex={2}
+      gap={12}
+      py={2}
+      display="grid"
+      gridTemplateColumns={
+        remainingPeers.length + 1 ? `repeat(2, 1fr)` : '.5fr'
+      }
+      gridAutoColumns="1fr"
+      gridAutoRows={'.5fr'}
+    >
+      <Speaker type="host" person={host} />
+      <Speaker type="speaker" person={us} />
+      {remainingPeers.map((person: string) => (
+        <Speaker key={person} type="speaker" person={person} />
+      ))}
+    </Flex>
+  );
+});
