bd5ddf1a6e186ffc09c2e3a2f10cfe0ce6dd3ca5
gdbroman <gustaf.broman@tink.com>
2022-12-21 17:22:33 +0100

    memoize webview to rm flicker

--- app/src/renderer/apps/Browser/WebView.tsx
+++ app/src/renderer/apps/Browser/WebView.tsx
@@ -1,5 +1,5 @@
+import { useCallback, useEffect, useMemo } from 'react';
 import { observer } from 'mobx-react';
-import { useCallback, useEffect } from 'react';
 import { Text } from 'renderer/components';
 import { useServices } from 'renderer/logic/store';
 import { useBrowser } from './store';
@@ -49,45 +49,55 @@ export const WebView = observer(({ isLocked }: Props) => {
     shell.setIsMouseInWebview(false);
   }, [shell]);
 
-  return (
-    <>
-      {loader.state === 'error' ? (
-        <Text
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            fontSize: 20,
-            fontWeight: 500,
-            zIndex: 1,
-          }}
-        >
-          Failed to load.
-        </Text>
-      ) : (
-        <webview
-          id={currentTab.id}
-          src={currentTab.url}
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
-          // This enables cursor injection, but it also crashes the webview on navigation.
-          // webpreferences="sandbox=false"
-          // preload={`file://${desktop.appviewPreload}`}
-          // @ts-expect-error
-          enableblinkfeatures="PreciseMemoryInfo, CSSVariables, AudioOutputDevices, AudioVideoTracks"
-          useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0"
-          partition="browser-webview"
-          style={{
-            background: 'white',
-            width: 'inherit',
-            height: 'calc(100% - 54px)',
-            position: 'relative',
-            marginTop: 54,
-            pointerEvents: isLocked ? 'none' : 'auto',
-          }}
-        />
-      )}
-    </>
+  return useMemo(
+    () => (
+      <>
+        {loader.state === 'error' ? (
+          <Text
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              fontSize: 20,
+              fontWeight: 500,
+              zIndex: 1,
+            }}
+          >
+            Failed to load.
+          </Text>
+        ) : (
+          <webview
+            id={currentTab.id}
+            src={currentTab.url}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            // This enables cursor injection, but it also crashes the webview on navigation.
+            // webpreferences="sandbox=false"
+            // preload={`file://${desktop.appviewPreload}`}
+            // @ts-expect-error
+            enableblinkfeatures="PreciseMemoryInfo, CSSVariables, AudioOutputDevices, AudioVideoTracks"
+            useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0"
+            partition="browser-webview"
+            style={{
+              background: 'white',
+              width: 'inherit',
+              height: 'calc(100% - 54px)',
+              position: 'relative',
+              marginTop: 54,
+              pointerEvents: isLocked ? 'none' : 'auto',
+            }}
+          />
+        )}
+      </>
+    ),
+    [
+      currentTab.id,
+      currentTab.url,
+      isLocked,
+      loader.state,
+      onMouseEnter,
+      onMouseLeave,
+    ]
   );
 });
