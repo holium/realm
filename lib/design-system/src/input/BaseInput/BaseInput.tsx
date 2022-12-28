f5150fab8f97b0a423b7df4ce3d5fa6ec56ab363
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2022-12-28 14:13:40 -0600

    semicolon

--- lib/design-system/src/input/BaseInput/BaseInput.tsx
+++ lib/design-system/src/input/BaseInput/BaseInput.tsx
@@ -46,7 +46,7 @@ export const InputBoxStyle = styled(Box)<InputBoxProps>`
   input {
     border-radius: var(--rlm-border-radius-4);
     background-color: var(--rlm-input-color);
-    color: var(--rlm-text-color)
+    color: var(--rlm-text-color);
     pointer-events: all;
     flex: 1;
     height: inherit;
