81ad350d6f2137f76789405035c61f56fd7e05de
~lodlev-migdev <patrick@holium.com>
2023-01-12 16:38:17 -0600

    basing next build version off of last build attempt tag instead of package.json

--- .github/workflows/scripts/pre-deploy.js
+++ .github/workflows/scripts/pre-deploy.js
@@ -20,8 +20,24 @@
 
 */
 var fs = require('fs');
-module.exports = ({ github, context }, pkgfile) => {
-  const pkg = JSON.parse(fs.readFileSync(pkgfile));
+const { release } = require('os');
+module.exports = async ({ github, context }, workflowId) => {
+  // disable this workflow to prevent multiple builds running simultaneously
+  console.log(
+    `disabling workflow ${workflowId} to prevent multiple simultaneous builds`
+  );
+  await octokit.request(
+    'PUT /repos/{owner}/{repo}/actions/workflows/{workflow_id}/disable',
+    {
+      owner: 'OWNER',
+      repo: 'REPO',
+      workflow_id: workflowId,
+    }
+  );
+  const packageFilename = './app/release/app/package.json';
+  // this script is always called at the root of the repo; therefore
+  //  read release package.json for build info
+  const pkg = JSON.parse(fs.readFileSync(packageFilename));
   let ci = {
     // releaseName - generated based on PR title (if exists); otherwise build based on package.json version
     releaseName: undefined,
@@ -40,7 +56,6 @@ module.exports = ({ github, context }, pkgfile) => {
     //     of the electron-builder library
     channel: undefined,
   };
-  console.log(`init.js: package version = ${pkg.version}`);
   // PR title is a required property of the PR event
   console.log(
     `init.js: PR title = '${context.payload.pull_request.title}'. testing if matches version format...`
@@ -65,14 +80,43 @@ module.exports = ({ github, context }, pkgfile) => {
     ci.version.build = parseInt(matches[5]);
     ci.channel = matches[1] === 'staging' ? 'alpha' : 'latest';
   } else {
+    let isNewBuild = false;
+    let buildVersion = undefined;
+    // grab the latest annotated tag of any kind (draft, prerelease, release), and interrogate it to determine
+    //  how to move forward
+    const releases = await github.rest.request(
+      'GET /repos/{owner}/{repo}/releases',
+      {
+        owner: 'holium',
+        repo: 'realm',
+        per_page: 1, // only give the last result
+        sort: 'created',
+        direction: 'desc',
+      }
+    );
+    if (releases.data.length > 0) {
+      const release = releases.data[0];
+      isNewBuild = !release.draft;
+      // if the latest release is a draft, it means the prior build failed; therefore
+      //  rerun the build using the same tag (version) information
+      buildVersion = release.tag_name;
+    } else {
+      // otherwise if no releases found, use the version string from package.json
+      buildVersion = pkg.version;
+      isNewBuild = true;
+    }
     // sanity check to ensure version coming in from package.json matches expected semantic version convention
-    matches = pkg.version.match(
+    matches = buildVersion.match(
       /(v|)(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
     );
-    if (!matches)
-      throw Error("package.json - 'version' attribute format unexpected");
-    // bump build # by 1
-    const buildNumber = parseInt(matches[4]) + 1;
+    if (!matches) throw Error("error: 'buildVersion' format unexpected");
+    const buildNumber = parseInt(matches[4]);
+    // only increment build # if last release was not a draft or no release found
+    //  therefore version value of first build ever should be '0.0.0' which would
+    //  build as version '0.0.1'
+    if (isNewBuild) {
+      buildNumber++;
+    }
     // if building from package.json version, bump the build # by 1
     ci.buildVersion = `${matches[1] ? 'v' : ''}${matches[2]}.${
       matches[3]
@@ -90,6 +134,9 @@ module.exports = ({ github, context }, pkgfile) => {
   pkg.version = `${ci.version.major}.${ci.version.minor}.${ci.version.build}${
     ci.channel === 'alpha' ? '-alpha' : ''
   }`;
-  fs.writeFileSync(pkgfile, JSON.stringify(pkg, null, 2));
+  console.log(`building version ${pkg.version}...`);
+  // must write build version string out to package.json since electron-builder
+  //   will use this to name assets
+  fs.writeFileSync(packageFilename, JSON.stringify(pkg, null, 2));
   return ci;
 };
