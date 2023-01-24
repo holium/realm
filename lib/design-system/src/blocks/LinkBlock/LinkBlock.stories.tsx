37cbc5af22372a76f6db63e4ec780c68cd98d74b
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2023-01-24 10:33:31 -0600

    open graph link fetching

--- lib/design-system/src/blocks/LinkBlock/LinkBlock.stories.tsx
+++ lib/design-system/src/blocks/LinkBlock/LinkBlock.stories.tsx
@@ -10,7 +10,7 @@ export const Default: ComponentStory<typeof LinkBlock> = () => (
   <Flex flexDirection="row" height={600} gap={16} p={1}>
     <Flex
       flexDirection="column"
-      width={432}
+      width={400}
       height={300}
       p={2}
       background={'#FFFF'}
@@ -23,7 +23,7 @@ export const Default: ComponentStory<typeof LinkBlock> = () => (
           'https://www.theverge.com/2023/1/23/23567333/spotify-layoffs-daniel-ek-cost-cutting',
         ]}
         by="~lomder-librun"
-        width={400}
+        width={350}
         // reference={{
         //   image: 'https://s.4cdn.org/image/favicon.ico',
         //   link: 'https://boards.4chan.org/pol/',
--- lib/design-system/src/blocks/LinkBlock/LinkBlock.tsx
+++ lib/design-system/src/blocks/LinkBlock/LinkBlock.tsx
@@ -1,7 +1,38 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import { Flex, Text } from '../..';
 import { BlockProps, Block } from '../Block/Block';
-// import { motion } from 'framer-motion';
+
+const OPENGRAPH_API = 'https://opengraph.holium.live/opengraph';
+
+const LinkImage = styled(motion.img)`
+  width: 100%;
+  height: 170px;
+  object-fit: cover;
+  border-radius: 2px;
+`;
+
+type OpenGraphType = {
+  twitterCard: string; // 'summary_large_image'
+  twitterSite: string; // '@verge';
+  ogSiteName: string; //'The Verge';
+  ogTitle: string; //'Spotify is laying off 6 percent of its global workforce, CEO announces';
+  ogDescription: string; // 'Impacting almost 600 employees.';
+  ogUrl: string; //'https://www.theverge.com/2023/1/23/23567333/spotify-layoffs-daniel-ek-cost-cutting';
+  ogType: string; //'article';
+  articlePublishedTime: string; //'2023-01-23T12:30:00.726Z';
+  articleModifiedTime: string; //'2023-01-23T12:30:00.726Z';
+  author: string; //'Jon Porter';
+  ogImage: {
+    url: string; //'https://cdn.vox-cdn.com/thumbor/TN-dCJzSsrzVGl4x4SgbBQJ1ajU=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/23951394/STK088_VRG_Illo_N_Barclay_1_spotify.jpg';
+    width: number | null;
+    height: number | null;
+    type: string | null;
+  };
+  requestUrl: string; //'https://www.theverge.com/2023/1/23/23567333/spotify-layoffs-daniel-ek-cost-cutting';
+  success: boolean; // true
+};
 
 type LinkBlockProps = {
   link: [string, string];
@@ -15,41 +46,59 @@ type LinkBlockProps = {
 
 export const LinkBlock: FC<LinkBlockProps> = (props: LinkBlockProps) => {
   const { link, by, metadata, ...rest } = props;
-  // TODO: fetch link metadata from server
+  const [openGraph, setOpenGraph] = useState<OpenGraphType | null>(null);
+
   useEffect(() => {
-    const url = link[1];
-  });
+    if (!openGraph) {
+      fetch(`${OPENGRAPH_API}?url=${encodeURIComponent(link[1])}`)
+        .then(async (res) => {
+          const data = await res.json();
+          console.log(data);
+          setOpenGraph(data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, []);
 
   return (
-    <Block width={rest.width || 'inherit'} {...rest}>
-      <Text.Custom fontSize={2} width={rest.width || 'inherit'}>
-        {link[0]}
-      </Text.Custom>
+    <Block p={0} width={rest.width || 'inherit'} {...rest}>
+      <LinkImage src={openGraph?.ogImage.url} />
+      <Flex gap={2} flexDirection="column">
+        <Text.Anchor
+          fontSize={2}
+          fontWeight={500}
+          width={rest.width || 'inherit'}
+          onClick={(evt: React.MouseEvent<HTMLAnchorElement>) => {
+            evt.stopPropagation();
+            window.open(openGraph?.ogUrl, '_blank');
+          }}
+        >
+          {openGraph?.ogTitle}
+        </Text.Anchor>
+        <Text.Custom fontSize={1} opacity={0.7} width={rest.width || 'inherit'}>
+          {openGraph?.ogDescription}
+        </Text.Custom>
+      </Flex>
       <Flex
         className="block-footer"
         flex={1}
         justifyContent="space-between"
         width="inherit"
       >
-        {/* <Flex flexDirection="row" gap={4} alignItems="center">
-          {reference.image && (
-            <motion.img
-              draggable={false}
-              src={reference.image}
-              height={14}
-              width={14}
-            />
-          )}
+        <Flex flexDirection="row" gap={4} alignItems="center">
           <Text.Anchor
             fontSize={0}
+            opacity={0.5}
             onClick={(evt: React.MouseEvent<HTMLAnchorElement>) => {
               evt.stopPropagation();
-              window.open(reference.link, '_blank');
+              window.open(openGraph?.ogUrl, '_blank');
             }}
           >
-            {reference.link}
+            {openGraph?.ogSiteName}
           </Text.Anchor>
-        </Flex> */}
+        </Flex>
 
         <Text.Custom className="block-author" noSelection fontSize={0}>
           {by}
