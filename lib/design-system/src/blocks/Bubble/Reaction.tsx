cc8566c40e9c4dce75a7e131f88f2c9817f70c31
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2023-02-17 04:43:48 -0600

    just added tsignore. The tsc check locally seems to be fine but in git it is not

--- lib/design-system/src/blocks/Bubble/Reaction.tsx
+++ lib/design-system/src/blocks/Bubble/Reaction.tsx
@@ -231,6 +231,7 @@ export const Reactions = (props: ReactionProps) => {
               evt.stopPropagation();
               onClick(reaction.emoji);
             }}
+            // @ts-ignore
             selected={reaction.by.includes(window.ship)}
           >
             <Emoji
