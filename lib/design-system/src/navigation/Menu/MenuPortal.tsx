871484803d2332a92cb1532c53e1d49d41923bca
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2023-02-20 06:57:18 -0600

    got the menu working and am starting to flesh out the various detail pages

--- lib/design-system/src/navigation/Menu/MenuPortal.tsx
+++ lib/design-system/src/navigation/Menu/MenuPortal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import { createPortal } from 'react-dom';
 
 type MenuPortalProps = {
   id: string;
@@ -10,15 +10,15 @@ type MenuPortalProps = {
 export const MenuPortal = ({ id, isOpen, children }: MenuPortalProps) => {
   const el = document.createElement('div');
   const menuRoot = document.getElementById('menu-root');
-  el?.setAttribute(
-    'style',
-    'position: absolute; top: 0px; left: 0px; z-index: 1000;'
-  );
+  // el?.setAttribute(
+  //   'style',
+  //   'position: absolute; top: 0px; left: 0px; z-index: 1000;'
+  // );
   useEffect(() => {
     if (isOpen) menuRoot?.appendChild(el);
     return () => {
       if (isOpen) menuRoot?.removeChild(el);
     };
   }, [id, isOpen]);
-  return ReactDOM.createPortal(children, el);
+  return createPortal(children, el);
 };
