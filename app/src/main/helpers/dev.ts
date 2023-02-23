4231cc150081f3efffab43b62f41376d8b557a74
gdbroman <99gustaf@gmail.com>
2023-02-23 10:26:39 +0100

    Preserve fullscreen state on `disable-isolation-mode`

--- app/src/main/helpers/dev.ts
+++ app/src/main/helpers/dev.ts
@@ -9,10 +9,15 @@ const registerListeners = (mainWindow: BrowserWindow) => {
     }
   });
   ipcMain.handle('enable-isolation-mode', () => {
+    if (mainWindow.isKiosk()) return;
     mainWindow.setKiosk(true);
   });
   ipcMain.handle('disable-isolation-mode', () => {
+    if (!mainWindow.isKiosk()) return;
+    // Preserve fullscreen state.
+    const isFullScreen = mainWindow.isFullScreen();
     mainWindow.setKiosk(false);
+    mainWindow.setFullScreen(isFullScreen);
   });
 };
 
