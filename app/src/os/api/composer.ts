9399e6f07abd312d276a49d13491b18b99928d6a
Leo Winiecki <leodwiniecki@outlook.com>
2023-03-09 19:51:06 -0800

    added api file

---
+++ app/src/os/api/composer.ts
@@ -0,0 +1,133 @@
+import { Conduit } from '@holium/conduit';
+import { cleanNounColor, removeHash } from '../../os/lib/color';
+
+export const FriendsApi = {
+  getContact: async (conduit: Conduit, ship: string) => {
+    const contact = await conduit.scry({
+      app: 'friends',
+      path: `/contact/${ship}`,
+    });
+    return {
+      ...contact,
+      color: contact.color && cleanNounColor(contact.color),
+    };
+  },
+  addSpace: async (conduit: Conduit, spacePath: string) => {
+    const preparedData = {
+      nickname: data.nickname,
+      color: removeHash(data.color),
+      avatar: data.avatar,
+      bio: data.bio || null,
+      cover: data.cover || null,
+    };
+    const payload = {
+      app: 'composer',
+      mark: 'composer-action',
+      json: {
+        'add-space': {
+          'space-path': spacePath,
+        },
+      },
+    };
+    return conduit.poke(payload);
+  },
+  removeSpace: async (conduit: Conduit, spacePath: string) => {
+    const payload = {
+      app: 'composer',
+      mark: 'composer-action',
+      json: {
+        'remove-space': {
+          'space-path': spacePath,
+        },
+      },
+    };
+    return conduit.poke(payload);
+  },
+  addStack: async (conduit: Conduit, spacePath: string, stack: any) => {
+    const payload = {
+      app: 'composer',
+      mark: 'composer-action',
+      json: {
+        'add-stack': {
+          'space-path': spacePath,
+          stack,
+        },
+      },
+    };
+    return conduit.poke(payload);
+  },
+  removeStack: async (conduit: Conduit, spacePath: string, stackId: string) => {
+    const payload = {
+      app: 'composer',
+      mark: 'composer-action',
+      json: {
+        'remove-stack': {
+          'space-path': spacePath,
+          'stack-id': stackId,
+        },
+      },
+    };
+    return conduit.poke(payload);
+  },
+  setCurrentStack: async (
+    conduit: Conduit,
+    spacePath: string,
+    stackId: string
+  ) => {
+    const payload = {
+      app: 'composer',
+      mark: 'composer-action',
+      json: {
+        'set-current-stack': {
+          'space-path': spacePath,
+          'stack-id': stackId,
+        },
+      },
+    };
+    return conduit.poke(payload);
+  },
+  setWindow: async (
+    conduit: Conduit,
+    spacePath: string,
+    stackId: string,
+    window: any
+  ) => {
+    const payload = {
+      app: 'composer',
+      mark: 'composer-action',
+      json: {
+        'set-window': {
+          'space-path': spacePath,
+          'stack-id': stackId,
+          window,
+        },
+      },
+    };
+    return conduit.poke(payload);
+  },
+  removeWindow: async (
+    conduit: Conduit,
+    spacePath: string,
+    stackId: string,
+    windowId: string
+  ) => {
+    const payload = {
+      app: 'composer',
+      mark: 'composer-action',
+      json: {
+        'remove-window': {
+          'space-path': spacePath,
+          'stack-id': stackId,
+          'window-id': windowId,
+        },
+      },
+    };
+    return conduit.poke(payload);
+  },
+  getAll: async (conduit: Conduit) => {
+    return await conduit.scry({
+      app: 'composer',
+      path: `/all`,
+    });
+  },
+};
