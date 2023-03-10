4b37194673e2b4883e8c94ac2ae955310d14ce6d
Leo Winiecki <leodwiniecki@outlook.com>
2023-03-09 19:59:31 -0800

    outlined mobx model

---
+++ app/src/os/services/shell/composer.model.ts
@@ -0,0 +1,40 @@
+import { types } from 'mobx-state-tree';
+
+const Bounds = types.model('Bounds', {
+  x: types.number,
+  y: types.number,
+  height: types.number,
+  width: types.number,
+});
+
+const Window = types.model('Window', {
+  id: types.identifier,
+  owner: types.string,
+  app: types.string,
+  path: types.string,
+  access: types.enumeration(['write', 'read']),
+  type: types.enumeration(['urbit', 'web', 'native']),
+  metadata: types.map(types.string),
+  zIndex: types.number,
+  bounds: Bounds,
+});
+
+const Stack = types.model('Stack', {
+  id: types.identifier,
+  host: types.string,
+  type: types.enumeration(['singleplayer', 'multiplayer']),
+  windows: types.array(Window),
+  metadata: types.map(types.string),
+});
+
+const Composition = types.model('Composition', {
+  id: types.identifier,
+  space: types.string,
+  current: types.string,
+  our: Stack,
+  stacks: types.map(Stack),
+});
+
+export const ComposerModel = types.model('ComposerModel', {
+  compositions: types.map(Composition),
+});
