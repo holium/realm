f04a5450914c428ba5f0bd6427f361fb6440d218
Daniel Zapata <djz24@case.edu>
2023-03-16 19:08:21 -0500

    remove chat-stuff from notification.service

--- app/src/os/services/chat/init.sql
+++ app/src/os/services/chat/init.sql
@@ -52,29 +52,3 @@ create table if not exists delete_logs
 
 create unique index if not exists delete_log_change_uindex
     on delete_logs (timestamp, change);
-
-create table if not exists  notifications
-(
-    id          integer not null,
-    app         TEXT not null,
-    path        TEXT not null,
-    type        TEXT not null,
-    title       TEXT,
-    content     TEXT,
-    image       TEXT,
-    buttons     text,
-    link        TEXT,
-    metadata    text,
-    created_at  integer not null,
-    updated_at  integer not null,
-    read_at     integer,
-    read        boolean,
-    dismissed_at  integer,
-    dismissed     boolean
-);
-
-create unique index if not exists notifications_id_uindex
-    on notifications (id);
-
-create index if not exists notifications_read_dismissed_index
-    on notifications (read, dismissed);
