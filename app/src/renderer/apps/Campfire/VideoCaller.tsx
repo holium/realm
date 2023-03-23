425efe193bb064e72c469b26b99d76718868a7e9
Leo Winiecki <leodwiniecki@outlook.com>
2023-03-23 08:57:37 -0700

    video display

--- app/src/renderer/apps/Campfire/VideoCaller.tsx
+++ app/src/renderer/apps/Campfire/VideoCaller.tsx
@@ -96,7 +96,7 @@ const CallerPresenter = (props: ICaller) => {
     }
   }, [contextMenuOptions, getOptions, person, setOptions, ship?.patp]);
 
-  const callerVideo = useRef(null);
+  const callerVideo = useRef();
   useEffect(() => {
     if (callerVideo.current) {
       if (peer?.videoTracks && peer?.videoTracks.size > 0) {
