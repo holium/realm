1ddca24f0719a2e42c7f413026ca3cb79f7d825c
AJ LaMarc <ajlamarc@gmail.com>
2023-03-24 13:17:03 -0400

    add lib to linter

--- lib/tome/src/index.ts
+++ lib/tome/src/index.ts
@@ -1,3 +1,2 @@
-// TODO probably don't export this type directly?
 export * from './types';
 export * from './tome';
--- lib/tome/src/tome.ts
+++ lib/tome/src/tome.ts
@@ -1,10 +1,6 @@
 import { TomeOptions } from './types';
 
 /* TODO add docstring */
-export const initTome = async (
-  urbit: boolean = false,
-  app?: string,
-  options: TomeOptions = {}
-) => {
-  return await window.electron.os.tome.initTome(urbit, app, options);
+export const initTome = async (app?: string, options: TomeOptions = {}) => {
+  return await window.electron.os.tome.initTome(app, options);
 };
