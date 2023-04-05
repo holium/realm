0e879717fe5244f14c0660fbde434efd6f90b8e7
gdbroman <99gustaf@gmail.com>
2023-04-05 12:30:13 +0200

    Responsiveness cleanups

--- onboarding/src/components/Page.tsx
+++ onboarding/src/components/Page.tsx
@@ -9,7 +9,7 @@ import { useNavigation } from '../util/useNavigation';
 
 const Main = styled.main`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
