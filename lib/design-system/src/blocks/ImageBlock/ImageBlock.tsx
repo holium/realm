7465e70e833e9ecf2f35ef9660fc90792bd0b35a
gdbroman <99gustaf@gmail.com>
2023-04-07 13:08:25 +0200

    photoswipe.css

--- lib/design-system/src/blocks/ImageBlock/ImageBlock.tsx
+++ lib/design-system/src/blocks/ImageBlock/ImageBlock.tsx
@@ -1,6 +1,6 @@
 import { MutableRefObject, useMemo, useState } from 'react';
 import { Item } from 'react-photoswipe-gallery';
-// import './photoswipe.module.css';
+import 'photoswipe/dist/photoswipe.css';
 import { Flex, Text } from '../../general';
 import { BlockProps, Block } from '../Block/Block';
 import { FragmentImage } from '../Bubble/fragment-lib';
