24384967648bc750da934c0fcdffc7a1bb5aa66c
Patrick Bergeron <patrick@holium.com>
2023-04-07 13:09:03 -0500

    ignore fancy quit handling if we're auto updating

--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -32,6 +32,7 @@ ElectronBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
   blocker.enableBlockingInSession(session.fromPartition('browser-webview'));
 });
 
+let checkingForUpdates = false;
 let mainWindow: BrowserWindow;
 
 if (process.defaultApp) {
@@ -216,23 +217,29 @@ const createMouseOverlayWindow = () => {
 };
 
 app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (!isMac) app.quit();
+  if (!checkingForUpdates) {
+    // Respect the OSX convention of having the application in memory even
+    // after all windows have been closed
+    if (!isMac) app.quit();
+  }
 });
-
 // quitting is complicated. We have to catch the initial quit signal, preventDefault() it,
 // do our cleanup, and then re-emit and actually quit it
 let lastQuitSignal: number = 0;
 app.on('before-quit', (event) => {
-  if (lastQuitSignal === 0) {
-    lastQuitSignal = new Date().getTime() - 1;
-    event.preventDefault();
-    mainWindow.webContents.send('realm.before-quit');
-    setTimeout(() => app.quit(), 500); // after half a second, we really do need to shut down
+  if (!checkingForUpdates) {
+    if (lastQuitSignal === 0) {
+      lastQuitSignal = new Date().getTime() - 1;
+      event.preventDefault();
+      mainWindow.webContents.send('realm.before-quit');
+      setTimeout(() => app.quit(), 500); // after half a second, we really do need to shut down
+    }
   }
 });
-ipcMain.on('realm.app.quit', app.quit);
+
+ipcMain.on('realm.app.quit', (_event) => {
+  if (!checkingForUpdates) app.quit();
+});
 
 ipcMain.on('download-url-as-file', (_event, { url }) => {
   const win = BrowserWindow.getFocusedWindow();
@@ -244,7 +251,9 @@ ipcMain.on('download-url-as-file', (_event, { url }) => {
 app
   .whenReady()
   .then(() => {
+    checkingForUpdates = true;
     new AppUpdater().checkForUpdates().then(() => {
+      checkingForUpdates = false;
       createWindow();
       createMouseOverlayWindow();
     });
