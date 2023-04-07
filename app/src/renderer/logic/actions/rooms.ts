6e6d0f22a6af76c420662ac4e10d99bc93153f78
Leo Winiecki <leodwiniecki@outlook.com>
2023-04-07 13:02:11 -0700

    removed rooms api file

--- app/src/renderer/logic/actions/rooms.ts
+++
@@ -1,7 +0,0 @@
-import { RoomsService } from 'os/services/tray/rooms.service';
-
-/**
- * RoomsActions for interfacing with core process
- */
-type RoomsActionType = typeof RoomsService.preload;
-export const RoomsActions: RoomsActionType = window.electron.os.tray.rooms;
