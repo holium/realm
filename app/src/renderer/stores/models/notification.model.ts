3e87727f5194b182b25d2d4defa980defcb9dd40
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2023-04-10 09:03:36 -0500

    fix lint error and remove more old code

--- app/src/renderer/stores/models/notification.model.ts
+++ app/src/renderer/stores/models/notification.model.ts
@@ -2,12 +2,9 @@
 import { flow, Instance, types, applySnapshot } from 'mobx-state-tree';
 import { NotifIPC } from '../ipc';
 
-// const sortByUpdatedAt = (a: ChatModelType, b: ChatModelType) => {
-//   return (
-//     (b.updatedAt || b.metadata.timestamp) -
-//     (a.updatedAt || a.metadata.timestamp)
-//   );
-// };
+const sortByUpdatedAt = (a: NotifMobxType, b: NotifMobxType) => {
+  return b.createdAt - a.createdAt;
+};
 
 const NotificationButtonModel = types.model('NotificationButtonModel', {
   label: types.string,
@@ -62,13 +59,19 @@ export const NotifStore = types
   })
   .views((self) => ({
     get undismissedNotifications() {
-      return self.notifications.filter((n) => !n.dismissed);
+      return self.notifications
+        .filter((n) => !n.dismissed)
+        .slice()
+        .sort(sortByUpdatedAt);
     },
     get unreadNotifications() {
       return self.notifications.filter((n) => !n.read);
     },
     get dismissedNotifications() {
-      return self.notifications.filter((n) => n.dismissed);
+      return self.notifications
+        .filter((n) => n.dismissed)
+        .slice()
+        .sort(sortByUpdatedAt);
     },
     get unreadCount() {
       return self.notifications.filter((n) => !n.read && !n.dismissed).length;
