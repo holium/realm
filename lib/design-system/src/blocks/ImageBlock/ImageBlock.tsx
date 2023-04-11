dd2a0bc023912475319d831a72a91805a99a3a69
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2023-04-11 07:58:16 -0500

    merged windowedlist

--- lib/design-system/src/blocks/ImageBlock/ImageBlock.tsx
+++ lib/design-system/src/blocks/ImageBlock/ImageBlock.tsx
@@ -2,6 +2,8 @@ import { useMemo, useState } from 'react';
 import { Flex, Text } from '../../general';
 import { BlockProps, Block } from '../Block/Block';
 import { FragmentImage } from '../Bubble/fragment-lib';
+import 'photoswipe/dist/photoswipe.css';
+import { Item } from 'react-photoswipe-gallery';
 
 type ImageBlockProps = {
   showLoader?: boolean;
@@ -23,6 +25,8 @@ export const ImageBlock = (props: ImageBlockProps) => {
     ...rest
   } = props;
   const [isLoaded, setIsLoaded] = useState(false);
+  const [naturalWidth, setNaturalWidth] = useState(320);
+  const [naturalHeight, setNaturalHeight] = useState(427);
   const parsedHeight = useMemo(
     () =>
       (height
@@ -45,24 +49,39 @@ export const ImageBlock = (props: ImageBlockProps) => {
 
   return (
     <Block variant={variant} width={width} {...rest}>
-      <FragmentImage
-        id={rest.id}
-        loading="eager"
-        {...(showLoader && { isSkeleton: !isLoaded })}
-        src={image}
-        height={parsedHeight}
-        width={parsedWidth}
-        draggable={false}
-        onLoad={() => {
-          if (showLoader) {
-            onImageLoaded && onImageLoaded();
-            setIsLoaded(true);
-          }
-        }}
-        onError={() => {
-          // setIsError(true);
-        }}
-      />
+      <Item
+        original={image}
+        thumbnail={image}
+        width={naturalWidth}
+        height={naturalHeight}
+      >
+        {({ ref, open }) => (
+          <FragmentImage
+            id={rest.id}
+            ref={ref as React.MutableRefObject<HTMLImageElement>}
+            loading="eager"
+            {...(showLoader && { isSkeleton: !isLoaded })}
+            src={image}
+            height={parsedHeight}
+            width={parsedWidth}
+            draggable={false}
+            onLoad={() => {
+              if (showLoader) {
+                onImageLoaded && onImageLoaded();
+                setIsLoaded(true);
+              }
+              const curr = ref && (ref.current as HTMLImageElement);
+              if (curr && curr.naturalWidth) setNaturalWidth(curr.naturalWidth);
+              if (curr && curr.naturalHeight)
+                setNaturalHeight(curr.naturalHeight);
+            }}
+            onClick={open}
+            onError={() => {
+              // setIsError(true);
+            }}
+          />
+        )}
+      </Item>
       {by && (
         <Flex className="block-footer">
           <Flex></Flex>
