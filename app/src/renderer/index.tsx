0be797e91105c84cdcae551fc4e8c606b2aeec0e
Patrick Bergeron <patrick@holium.com>
2023-04-18 18:49:36 -0500

    remove console.log calls for Sentry and also readd code that ensures SENTRY_DSN environment variable set before attempting to enable

--- app/src/renderer/index.tsx
+++ app/src/renderer/index.tsx
@@ -8,64 +8,59 @@ import { App } from './App';
 const environment = process.env.NODE_ENV;
 const isProd = environment === 'production';
 
-// const sentryDsn = process.env.SENTRY_DSN;
-// if (sentryDsn) {
-let cfg: Sentry.BrowserOptions = {
-  environment,
-  dsn: 'https://56fbf5e600db48cf8a785931be1ca5e4@o1327359.ingest.sentry.io/4504310987358208',
-  integrations: [
-    new BrowserTracing(),
-    new RewriteFramesIntegration({
-      // prefix: 'app://',
-      // function that takes the frame, applies a transformation, and returns it
-      iteratee: (frame: any) => {
-        // example frame contents from crash
-        // {
-        //   "function": "HTMLDivElement.r",
-        //   "filename": "/Applications/Realm.app/Contents/Resources/app.asar/dist/renderer/app.renderer.js",
-        //   "abs_path": "file:///Applications/Realm.app/Contents/Resources/app.asar/dist/renderer/app.renderer.js",
-        //   "lineno": 2,
-        //   "colno": 397144,
-        //   "in_app": true
-        // },
-        console.log('frame => %o', frame);
-        if (frame.filename) {
-          // strip everything between the file:// and the /dist folder
-          const filename = frame.filename;
-          console.log('mapping frame');
-          const idx = filename.lastIndexOf('/');
-          if (idx !== 1) {
-            frame.filename = `app:///${filename.substring(idx + 1)}`;
-          } else {
-            frame.filename = `app:///${filename}`;
+const sentryDsn = process.env.SENTRY_DSN;
+if (sentryDsn) {
+  let cfg: Sentry.BrowserOptions = {
+    environment,
+    dsn: 'https://56fbf5e600db48cf8a785931be1ca5e4@o1327359.ingest.sentry.io/4504310987358208',
+    integrations: [
+      new BrowserTracing(),
+      new RewriteFramesIntegration({
+        // prefix: 'app://',
+        // function that takes the frame, applies a transformation, and returns it
+        iteratee: (frame: any) => {
+          // example frame contents from crash
+          // {
+          //   "function": "HTMLDivElement.r",
+          //   "filename": "/Applications/Realm.app/Contents/Resources/app.asar/dist/renderer/app.renderer.js",
+          //   "abs_path": "file:///Applications/Realm.app/Contents/Resources/app.asar/dist/renderer/app.renderer.js",
+          //   "lineno": 2,
+          //   "colno": 397144,
+          //   "in_app": true
+          // },
+          if (frame.filename) {
+            // strip everything between the file:// and the /dist folder
+            const filename = frame.filename;
+            const idx = filename.lastIndexOf('/');
+            if (idx !== 1) {
+              frame.filename = `app:///${filename.substring(idx + 1)}`;
+            } else {
+              frame.filename = `app:///${filename}`;
+            }
           }
-          console.log(
-            `mapping stack trace frame '%o' to '%o`,
-            filename,
-            frame.filename
-          );
-        }
-        return frame;
-      },
-    }),
-  ],
-  tracesSampleRate: 1,
-  ignoreErrors: [
-    // Remove when we've done the window system refactor and are no longer using webviews.
-    'GUEST_VIEW_MANAGER_CALL',
-    // False alarm caused by react-dom/server renderToString.
-    'useLayoutEffect',
-  ],
-};
-if (process.env.BUILD_VERSION) {
-  console.log(`Initializing Sentry [release: ${process.env.BUILD_VERSION}]...`);
-  cfg.release = process.env.BUILD_VERSION;
-  cfg.dist = process.env.BUILD_VERSION;
+          return frame;
+        },
+      }),
+    ],
+    tracesSampleRate: 1,
+    ignoreErrors: [
+      // Remove when we've done the window system refactor and are no longer using webviews.
+      'GUEST_VIEW_MANAGER_CALL',
+      // False alarm caused by react-dom/server renderToString.
+      'useLayoutEffect',
+    ],
+  };
+  if (process.env.BUILD_VERSION) {
+    console.log(
+      `Initializing Sentry [release: ${process.env.BUILD_VERSION}]...`
+    );
+    cfg.release = process.env.BUILD_VERSION;
+    cfg.dist = process.env.BUILD_VERSION;
+  }
+  Sentry.init(cfg);
+} else {
+  console.warn('Environment variable for Sentry is undefined.');
 }
-Sentry.init(cfg);
-// } else {
-//   console.warn('Environment variable for Sentry is undefined.');
-// }
 const amplitudeApiKey = isProd
   ? process.env.AMPLITUDE_API_KEY
   : process.env.AMPLITUDE_API_KEY_DEV;
