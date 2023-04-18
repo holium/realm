9d3444c11a381bb57fc724a5131a69b5940f7d6b
Patrick Bergeron <patrick@holium.com>
2023-04-18 18:46:34 -0500

    remove intentional crash needed for testing

--- app/src/renderer/apps/System/components/About.tsx
+++ app/src/renderer/apps/System/components/About.tsx
@@ -45,9 +45,6 @@ const AboutPanelPresenter = () => {
           ]}
           selected={selectedChannel}
           onClick={(channel: string) => {
-            const val = undefined;
-            // @ts-ignore
-            val.data = 'test';
             setSelectedChannel(channel);
             DesktopActions.setReleaseChannel(channel);
           }}
