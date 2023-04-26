2dd126e6eca1abd6005f6e9c861b310f5629b570
gdbroman <99gustaf@gmail.com>
2023-04-26 19:21:06 -0300

    Destructure props in signature

--- lib/design-system/src/input/InlineEdit/InlineEdit.tsx
+++ lib/design-system/src/input/InlineEdit/InlineEdit.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import { useRef } from 'react';
 import { StyledComponentProps } from 'styled-components';
 
 import { BoxProps, Flex } from '../../../general';
@@ -11,48 +11,57 @@ type InlineEditProps = {
 } & BoxProps &
   StyledComponentProps<'input', any, any, never>;
 
-export const InlineEdit: any = forwardRef<HTMLInputElement, InlineEditProps>(
-  ({ flex, mb, mt, mx, my, ml, mr, disabled, ...props }, ref) => {
-    if (!ref) ref = React.createRef();
-    const keypressHandler = (event: any) => {
-      if (event.key === 'Enter') {
-        // @ts-expect-error
-        ref.current.blur();
-      }
-    };
-    return (
-      <Flex
-        alignItems="center"
-        position="relative"
-        mx={mx}
-        my={my}
-        mb={mb}
-        mt={mt}
-        ml={ml}
-        mr={mr}
-        flex={flex}
-      >
-        <TextInput
-          id={props.id}
-          name={props.id}
-          ref={ref}
-          py={2}
-          disabled={disabled}
-          aria-invalid={props.error ? 'true' : 'false'}
-          onKeyDown={(event: any) => keypressHandler(event)}
-          fontWeight={props.fontWeight}
-          textAlign={props.textAlign}
-          width={props.width as number}
-          value={props.value}
-          onBlur={props.onBlur}
-          onChange={props.onChange}
-        />
-      </Flex>
-    );
-  }
-);
+export const InlineEdit = ({
+  id,
+  fontWeight,
+  textAlign,
+  width,
+  value,
+  flex,
+  mb,
+  mt,
+  mx,
+  my,
+  ml,
+  mr,
+  error = false,
+  disabled,
+  onBlur,
+  onChange,
+}: InlineEditProps) => {
+  const ref = useRef<HTMLInputElement>(null);
 
-InlineEdit.defaultProps = {
-  error: false,
-  type: 'text',
+  const keypressHandler = (event: any) => {
+    if (event.key === 'Enter') ref.current?.blur();
+  };
+
+  return (
+    <Flex
+      alignItems="center"
+      position="relative"
+      mx={mx}
+      my={my}
+      mb={mb}
+      mt={mt}
+      ml={ml}
+      mr={mr}
+      flex={flex}
+    >
+      <TextInput
+        id={id}
+        name={id}
+        ref={ref}
+        py={2}
+        disabled={disabled}
+        aria-invalid={error ? 'true' : 'false'}
+        onKeyDown={(event: any) => keypressHandler(event)}
+        fontWeight={fontWeight}
+        textAlign={textAlign}
+        width={width as number}
+        value={value}
+        onBlur={onBlur}
+        onChange={onChange}
+      />
+    </Flex>
+  );
 };
