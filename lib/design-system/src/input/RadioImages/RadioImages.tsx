557373c758235c088c9f9660261bf52fb51cfdc7
gdbroman <99gustaf@gmail.com>
2023-05-01 09:15:13 -0500

    Fix wallpaper settings

--- lib/design-system/src/input/RadioImages/RadioImages.tsx
+++ lib/design-system/src/input/RadioImages/RadioImages.tsx
@@ -1,53 +1,52 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 import { Flex } from '../../general/Flex/Flex';
 import { RadioHighlight, RadioImage } from './RadioImages.style';
 
-type RadioImageOption = {
-  value: string;
-  imageSrc: string;
-};
-
 type Props = {
-  style?: any;
-  options: RadioImageOption[];
-  selected?: string;
-  onClick: (value: any) => void;
+  options: string[];
+  onClick: (value: string) => void;
 };
 
-export const RadioImages = ({
-  style,
-  options = [],
-  selected,
-  onClick,
-}: Props) => (
-  <Flex
-    p={1}
-    mb={10}
-    gap={6}
-    borderRadius={6}
-    maxWidth="100%"
-    flexDirection="row"
-    width="fit-content"
-    justifyContent="flex-start"
-    overflowX="auto"
-    style={style}
-  >
-    {options?.map((option) => {
-      const isSelected = option.value === selected;
-      return (
-        <motion.div
-          key={option.value}
-          style={{ position: 'relative', padding: '6px 6px' }}
-        >
-          {isSelected && <RadioHighlight selected />}
-          <RadioImage
-            src={option.imageSrc}
-            selected={isSelected}
-            onClick={(_evt: any) => onClick(option.value)}
-          ></RadioImage>
-        </motion.div>
-      );
-    })}
-  </Flex>
-);
+export const RadioImages = ({ options, onClick }: Props) => {
+  const [selected, setSelected] = useState<string>();
+
+  const onClickRadioImage = (value: string) => {
+    setSelected(value);
+    onClick(value);
+  };
+
+  return (
+    <Flex
+      p={1}
+      mb={10}
+      gap={6}
+      borderRadius={6}
+      maxWidth="100%"
+      flexDirection="row"
+      width="fit-content"
+      justifyContent="flex-start"
+      overflowX="auto"
+      background="rgba(var(--rlm-overlay-hover-rgba))"
+    >
+      {options?.map((option) => {
+        const isSelected = option === selected;
+
+        return (
+          <motion.div
+            key={option}
+            style={{ position: 'relative', padding: '6px 6px' }}
+          >
+            <RadioHighlight selected={isSelected} />
+            <RadioImage
+              src={option}
+              selected={isSelected}
+              onClick={() => onClickRadioImage(option)}
+            />
+          </motion.div>
+        );
+      })}
+    </Flex>
+  );
+};
