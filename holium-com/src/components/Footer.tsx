1dfa140b4e633a2195dda5431eb4fd77727bf50e
gdbroman <99gustaf@gmail.com>
2023-05-05 15:38:40 -0500

    Responsive layout

---
+++ holium-com/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import { MOBILE_WIDTH } from '../consts';
+
+const FooterContainer = styled.header`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+
+  @media (max-width: ${MOBILE_WIDTH}px) {
+    display: none;
+  }
+`;
+
+export const Footer = () => <FooterContainer>Systembar</FooterContainer>;
