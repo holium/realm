5badcbd03638866f40096827990cfc20f33e1983
gdbroman <99gustaf@gmail.com>
2023-05-06 14:54:24 -0500

    HoveringCursors

--- holium-com/src/consts.ts
+++ holium-com/src/consts.ts
@@ -1,4 +1,5 @@
 export const MOBILE_WIDTH = 560;
+export const DESKTOP_WIDTH = 1480;
 
 export const onClickLogin = () => {
   window.location.href = 'https://hosting.holium.com/login';
--- holium-com/src/pages/index.tsx
+++ holium-com/src/pages/index.tsx
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 import { Button, Flex, Icon, Text } from '@holium/design-system/general';
 
 import { H1 } from '../components/H1';
+import { HoveringCursors } from '../components/HoveringCursors';
 import { Page } from '../components/Page';
-import { onClickGetRealm } from '../consts';
+import { DESKTOP_WIDTH, onClickGetRealm } from '../consts';
 
 const GetRealmButton = styled(Button.Primary)`
   display: flex;
@@ -15,17 +16,37 @@ const GetRealmButton = styled(Button.Primary)`
   margin-top: 16px;
 `;
 
+const HeroContainer = styled(Flex)`
+  width: 100%;
+  max-width: ${DESKTOP_WIDTH}px;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: ${DESKTOP_WIDTH}px) {
+    flex-direction: column;
+    text-align: center;
+    max-width: 814px;
+  }
+`;
+
+const H1Container = styled(Flex)`
+  flex: 1;
+  flex-direction: column;
+  align-self: flex-start;
+  gap: 16px;
+  padding: 16px;
+
+  @media (max-width: ${DESKTOP_WIDTH}px) {
+    align-items: center;
+    align-self: center;
+  }
+`;
+
 export default function CreateAccount() {
   return (
     <Page title="Holium">
-      <Flex width="100%" maxWidth="1480px" justifyContent="flex-start">
-        <Flex
-          flexDirection="column"
-          alignSelf="flex-start"
-          maxWidth="740px"
-          gap="16px"
-          padding="16px"
-        >
+      <HeroContainer>
+        <H1Container>
           <H1 />
           <Text.Body fontSize={28}>
             A home for communities, a platform for building new social
@@ -35,8 +56,11 @@ export default function CreateAccount() {
             Get Realm
             <Icon name="ArrowRightLine" fill="accent" />
           </GetRealmButton>
+        </H1Container>
+        <Flex flex={1} justifyContent="center" padding="16px">
+          <HoveringCursors />
         </Flex>
-      </Flex>
+      </HeroContainer>
     </Page>
   );
 }
