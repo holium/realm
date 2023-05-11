bffeac36cb3a549031eb95bc5b8272b87856de6d
gdbroman <99gustaf@gmail.com>
2023-05-10 19:33:35 -0500

    Unpin web apps from right click

--- app/src/renderer/system/desktop/components/SystemBar/components/CommunityBar/AppDock/PinnedWebApp.tsx
+++ app/src/renderer/system/desktop/components/SystemBar/components/CommunityBar/AppDock/PinnedWebApp.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Reorder } from 'framer-motion';
 import { observer } from 'mobx-react';
 
 import { Flex } from '@holium/design-system/general';
 
 import { useBrowser } from 'renderer/apps/Browser/store';
+import { ContextMenuOption, useContextMenu } from 'renderer/components';
 import { useAppState } from 'renderer/stores/app.store';
+import { SpacesIPC } from 'renderer/stores/ipc';
+import { useShipStore } from 'renderer/stores/ship.store';
 
 const getFavicon = (url: string) => {
   const { protocol, host } = new URL(url);
@@ -20,8 +23,10 @@ type Props = {
 const PinnedWebAppPresenter = ({ url }: Props) => {
   const { setUrl } = useBrowser();
   const { shellStore } = useAppState();
+  const { spacesStore } = useShipStore();
 
   const [favicon, setFavicon] = useState<string | null>(getFavicon(url));
+  const { getOptions, setOptions } = useContextMenu();
 
   // First uppercase letter of the website name.
   // Remove any protocol and www. from the url.
@@ -30,6 +35,25 @@ const PinnedWebAppPresenter = ({ url }: Props) => {
     .replace('www.', '')
     .substring(0, 1)
     .toUpperCase();
+
+  const tileId = useMemo(() => `pinned-web-app-${url}`, [url]);
+  const spacePath = useMemo(
+    () => spacesStore.selected?.path,
+    [spacesStore.selected]
+  );
+
+  const contextMenuOptions: ContextMenuOption[] = useMemo(
+    () => [
+      {
+        id: 'unpin-web-app',
+        label: 'Unpin',
+        onClick: () => {
+          spacePath && SpacesIPC.removeBookmark(spacePath, url);
+        },
+      },
+    ],
+    [spacePath, url]
+  );
 
   const onClick = () => {
     const appId = 'os-browser';
@@ -49,9 +73,16 @@ const PinnedWebAppPresenter = ({ url }: Props) => {
     setUrl(url);
   };
 
+  useEffect(() => {
+    if (contextMenuOptions !== getOptions(tileId)) {
+      setOptions(tileId, contextMenuOptions);
+    }
+  }, [contextMenuOptions, getOptions, setOptions, tileId]);
+
   return (
     <Reorder.Item key={url} value={url}>
       <Flex
+        id={tileId}
         style={{
           width: 32,
           height: 32,
@@ -72,6 +103,7 @@ const PinnedWebAppPresenter = ({ url }: Props) => {
               width: 16,
               height: 16,
               borderRadius: 4,
+              pointerEvents: 'none',
             }}
             onError={() => setFavicon(null)}
           />
