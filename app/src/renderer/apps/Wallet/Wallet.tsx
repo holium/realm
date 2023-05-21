c2cd513514541b167f934cde3f37c6443c0a180c
gdbroman <99gustaf@gmail.com>
2023-05-21 12:02:55 -0500

    Make Wallet Reactive

--- app/src/renderer/apps/Wallet/Wallet.tsx
+++ app/src/renderer/apps/Wallet/Wallet.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 
 import { Flex } from '@holium/design-system/general';
+import { useToggle } from '@holium/design-system/util';
 
 import { NetworkType } from 'os/services/ship/wallet/wallet.types';
 import {
@@ -19,59 +20,65 @@ import { walletScreens } from './screens/walletScreens';
 import { WalletScreen } from './types';
 
 const WalletPresenter = () => {
-  const [hidePending, setHidePending] = useState(true);
-
   const { walletStore } = useShipStore();
 
-  let transactions: TransactionType[] = [];
+  const showPending = useToggle(false);
 
-  for (const key of walletStore.currentStore.wallets.keys()) {
-    const wallet = walletStore.currentStore.wallets.get(key);
-    if (!wallet) continue;
-    const walletTransactions = getTransactions(
-      (walletStore.navState.network === NetworkType.ETHEREUM
-        ? (wallet as EthWalletType).data.get(walletStore.navState.protocol)
-            ?.transactionList.transactions
-        : (wallet as BitcoinWalletType).transactionList.transactions) ||
-        new Map()
-    );
-    if (walletStore.navState.network === NetworkType.ETHEREUM) {
-      for (const key of walletStore.currentStore.wallets.keys()) {
-        const coinKeys = (
-          walletStore.currentStore.wallets.get(key) as EthWalletType
-        ).data
-          .get(walletStore.navState.protocol)
-          ?.coins.keys();
-        if (!coinKeys) continue;
+  const [transactions, setTransactions] = useState<TransactionType[]>([]);
 
-        for (const coin of coinKeys) {
-          const coinTransactions = (
+  useEffect(() => {
+    for (const key of walletStore.currentStore.wallets.keys()) {
+      const wallet = walletStore.currentStore.wallets.get(key);
+      if (!wallet) continue;
+      const walletTransactions = getTransactions(
+        (walletStore.navState.network === NetworkType.ETHEREUM
+          ? (wallet as EthWalletType).data.get(walletStore.navState.protocol)
+              ?.transactionList.transactions
+          : (wallet as BitcoinWalletType).transactionList.transactions) ||
+          new Map()
+      );
+      if (walletStore.navState.network === NetworkType.ETHEREUM) {
+        for (const key of walletStore.currentStore.wallets.keys()) {
+          const coinKeys = (
             walletStore.currentStore.wallets.get(key) as EthWalletType
           ).data
             .get(walletStore.navState.protocol)
-            ?.coins.get(coin)
-            ?.transactionList.transactions.values();
-          if (!coinTransactions) continue;
-          transactions = [...coinTransactions, ...transactions];
+            ?.coins.keys();
+          if (!coinKeys) continue;
+
+          for (const coin of coinKeys) {
+            const coinTransactions = (
+              walletStore.currentStore.wallets.get(key) as EthWalletType
+            ).data
+              .get(walletStore.navState.protocol)
+              ?.coins.get(coin)
+              ?.transactionList.transactions.values();
+            if (!coinTransactions) continue;
+            setTransactions([...coinTransactions, ...transactions]);
+          }
         }
       }
+      setTransactions([...walletTransactions, ...transactions]);
     }
-    transactions = [...walletTransactions, ...transactions];
-  }
-  const pending = transactions.filter((tx) => tx.status === 'pending').length;
+  }, [walletStore.currentStore.wallets]);
 
   useEffect(() => {
-    if (pending > 0) {
-      setHidePending(false);
-    }
-    if (pending === 0 && !hidePending) {
-      setHidePending(true);
-    }
-  }, [pending]);
+    const pendingTransactions = transactions.filter(
+      (tx) => tx.status === 'pending'
+    ).length;
 
-  const hide = () => {
-    setHidePending(true);
-  };
+    const isTransactionScreen = [
+      WalletScreen.LIST,
+      WalletScreen.WALLET_DETAIL,
+      WalletScreen.NFT_DETAIL,
+    ].includes(walletStore.navState.view);
+
+    if (pendingTransactions > 0 && isTransactionScreen) {
+      showPending.toggleOn();
+    } else {
+      showPending.toggleOff();
+    }
+  }, [transactions, walletStore.navState.view]);
 
   const hideFooter = [
     WalletScreen.ONBOARDING,
@@ -86,19 +93,12 @@ const WalletPresenter = () => {
     WalletScreen.FORGOT_PASSCODE,
   ].includes(walletStore.navState.view);
 
-  const pendingIsVisible = [
-    WalletScreen.LIST,
-    WalletScreen.WALLET_DETAIL,
-    WalletScreen.TRANSACTION_DETAIL,
-    WalletScreen.NFT_DETAIL,
-  ].includes(walletStore.navState.view);
-
   const walletScreenComponent =
     walletStore.navState.view === WalletScreen.TRANSACTION_CONFIRM
       ? WalletScreen.TRANSACTION_SEND
       : walletStore.navState.view;
 
-  const WalletScreenCurrent = walletScreens[walletScreenComponent];
+  const CurrentWalletScreen = walletScreens[walletScreenComponent];
 
   return (
     <Flex
@@ -119,12 +119,13 @@ const WalletPresenter = () => {
           onSetNetwork={(network) => walletStore.setNetwork(network)}
         />
       )}
-      {pendingIsVisible &&
-        !hidePending &&
-        walletStore.navState.view !== WalletScreen.TRANSACTION_DETAIL && (
-          <PendingTransactionDisplay transactions={transactions} hide={hide} />
-        )}
-      <WalletScreenCurrent network={walletStore.navState.network} />
+      {showPending.isOn && (
+        <PendingTransactionDisplay
+          transactions={transactions}
+          hide={showPending.toggleOff}
+        />
+      )}
+      <CurrentWalletScreen network={walletStore.navState.network} />
       {!hideFooter && <WalletFooter />}
     </Flex>
   );
