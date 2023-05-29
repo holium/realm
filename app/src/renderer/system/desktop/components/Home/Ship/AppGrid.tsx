e7f5de8452c7260b7cda5e223119246d1b32cd62
DrunkPlato <6413077+drunkplato@users.noreply.github.com>
2023-05-29 14:37:18 -0500

    fixed linter issues

--- app/src/renderer/system/desktop/components/Home/Ship/AppGrid.tsx
+++ app/src/renderer/system/desktop/components/Home/Ship/AppGrid.tsx
@@ -36,14 +36,14 @@ const AppGridPresenter = ({ maxWidth }: AppGridProps) => {
   const canClick = useToggle(true);
 
   useEffect(() => {
-    window.electron.app.onMouseMove((position, state, isDragging) => {
+    window.electron.app.onMouseMove((_position, _state, isDragging) => {
       canClick.setToggle(!isDragging);
     });
   }, []);
 
   if (!currentSpace) return null;
 
-  const onChange = (_, sourceIndex, targetIndex) => {
+  const onChange = (_: any, sourceIndex: number, targetIndex: number) => {
     if (sourceIndex === targetIndex) return;
     bazaarStore.reorderApp(sourceIndex, targetIndex);
     const nextState = swap(items, sourceIndex, targetIndex);
