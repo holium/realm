dc6fe81cd0f1faeefd9fdc167a24612d69bc8fba
gdbroman <99gustaf@gmail.com>
2023-06-01 17:07:51 -0500

    Move out passport milestone

--- holium-com/src/components/about/HoliumRoadmapSection.tsx
+++ holium-com/src/components/about/HoliumRoadmapSection.tsx
@@ -61,45 +61,40 @@ export const HoliumRoadmapSection = () => (
     <TimelineContainer>
       <Date date="06/2023" />
       <Milestone
-        index={0}
+        index={1}
         title="Realm iOS"
         description="We’ve been working on the mobile app for months and are finally ready to launch the alpha."
-      />
-      <Milestone
-        index={1}
-        title="Passports"
-        description="An identity system that makes onboarding and NFT gating simple and robust."
-        rightSide
       />
       <Date date="07/2023" />
       <Milestone
         index={2}
         title="Relic Browser 2.0"
         description="Browser tabs, shared bookmarks, and more."
+        rightSide
       />
       <Milestone
         index={3}
         title="Developer preview"
         description="Libraries for building on Realm will be released, starting with rooms and then wallet."
-        rightSide
       />
       <Milestone
         index={4}
         title="Mobile rooms in Realm iOS"
         description="Drop into voice chat with friends in your space while on the move."
+        rightSide
       />
       <Date date="08/2023" />
       <Milestone
         index={5}
         title="Realm Android"
         description="An identity system that makes onboarding and NFT gating simple and robust."
-        rightSide
       />
       <Date date="09/2023" />
       <Milestone
         index={6}
         title="Bedrock SDK"
         description="A data primitives layer that makes application data much more composable."
+        rightSide
       />
       <Date date="10/2023" />
     </TimelineContainer>
