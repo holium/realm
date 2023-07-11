ebc4b4cc234d3eb619574628d3f7503fa5a4a3a4
gdbroman <99gustaf@gmail.com>
2023-07-11 21:35:10 +0200

    Fix duplicate sentences and word defs

--- lexicon/src/components/WordDefinitions.tsx
+++ lexicon/src/components/WordDefinitions.tsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
 
-import { Button, Flex, TextInput } from '@holium/design-system';
+import { Button, Flex } from '@holium/design-system/general';
+import { TextInput } from '@holium/design-system/inputs';
+import { useToggle } from '@holium/design-system/util';
 
 import { DefinitionRow } from '../api/types/bedrock';
 import { Store, useStore } from '../store';
 import { log } from '../utils';
 import { WordDefinitionElement } from './WordDefinitionElement';
 
-export const WordDefinitions = ({ definitionList, state, space }: any) => {
+type Props = {
+  space: string | null;
+  state: any;
+  definitionList: DefinitionRow[];
+};
+
+export const WordDefinitions = ({ definitionList, state, space }: Props) => {
   const api = useStore((store: Store) => store.api);
 
   const [newDefinition, setNewDefinition] = useState<string>('');
   const definitionVoteMap = useStore((state: Store) => state.definitionVoteMap);
 
+  const submitting = useToggle(false);
+
   const handleSubmitNewDefinition = async () => {
     if (!space) return;
+    submitting.toggleOn();
     try {
       const wordId = state.id;
 
@@ -29,21 +40,28 @@ export const WordDefinitions = ({ definitionList, state, space }: any) => {
     } catch (e) {
       log('handleSubmitNewDefinition error => ', e);
     }
+    submitting.toggleOff();
   };
   return (
     <>
       <Flex flexDirection="column" gap={20}>
-        {definitionList.map((item: DefinitionRow, index: number) => {
-          const votes = definitionVoteMap.get(item.id);
-          return (
-            <WordDefinitionElement
-              id={item.id}
-              text={item.definition}
-              key={'definition-item-' + index}
-              votes={votes}
-            />
-          );
-        })}
+        {definitionList
+          // Filter duplicate ids
+          .filter(
+            (definition, i, self) =>
+              i === self.findIndex((w) => w.id === definition.id)
+          )
+          .map((item: DefinitionRow, index: number) => {
+            const votes = definitionVoteMap.get(item.id);
+            return (
+              <WordDefinitionElement
+                id={item.id}
+                text={item.definition}
+                key={'definition-item-' + index}
+                votes={votes}
+              />
+            );
+          })}
       </Flex>
 
       <Flex flexDirection={'column'} gap={10} marginTop={'auto'}>
@@ -65,7 +83,8 @@ export const WordDefinitions = ({ definitionList, state, space }: any) => {
         <Button.TextButton
           fontSize={1}
           fontWeight={500}
-          alignSelf={'flex-end'}
+          alignSelf="flex-end"
+          disabled={submitting.isOn}
           onClick={handleSubmitNewDefinition}
         >
           Submit
