886b91cd43f8f7ebc239feedcd169fe360b28185
haroldthe24th <amrirachid15@gmail.com>
2023-08-28 18:50:48 +0100

    -fixed issue with updates in Lexicon

--- lexicon/src/api/updates.ts
+++ lexicon/src/api/updates.ts
@@ -7,35 +7,40 @@ export const updateHandler = (update: any) => {
   let actionName;
   try {
     //add row type (probably other ones too)
-    actionName = update[0].change + '-' + update[0].row.type;
+    actionName = update[0].change + update[0].row.type;
   } catch (e) {
     //delete row type
-    actionName = update[0].change + '-' + update[0].type;
+    actionName = update[0].change + update[0].type;
   }
+  /*
+   These "row-types" (ex: /lexicon-word/0v4.fao37.8424p.trcih.kr5je.80jnn) 
+   are predefined in lexicon.services.ts as enum LEXICON_BEDROCK_TYPES
+   and are treated as static strings
+  */
 
   if (actionName) {
     switch (actionName) {
-      case 'add-row-lexicon-word': {
+      case 'add-row/lexicon-word/0v4.fao37.8424p.trcih.kr5je.80jnn': {
         store.addWordRow(update[0].row);
         break;
       }
-      case 'del-row-lexicon-word': {
+      case 'del-row/lexicon-word/0v4.fao37.8424p.trcih.kr5je.80jnn': {
         store.removeWordRow(update[0].id);
         break;
       }
-      case 'add-row-vote': {
+      case 'add-row/vote/0v3.hirga.bspbd.edlma.dfk59.gtu38': {
         store.addVoteRow(update[0].row);
         break;
       }
-      case 'del-row-vote': {
+      case 'del-row/vote/0v3.hirga.bspbd.edlma.dfk59.gtu38': {
         store.removeVoteRow(update[0].id);
         break;
       }
-      case 'add-row-lexicon-definition': {
+      case 'add-row/lexicon-definition/0v3.omons.hajqb.643gt.jd474.8qoj1': {
         store.addDefinitionRow(update[0].row);
         break;
       }
-      case 'add-row-lexicon-sentence': {
+      case 'add-row/lexicon-sentence/0v948nd.km7ue.1brbd.vqei5.hktbo': {
         store.addSentenceRow(update[0].row);
         break;
       }
