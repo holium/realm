6c00716e47e4113b57f84cad0ab0f1ce42515f7a
Patrick Bergeron <patrick@holium.com>
2023-09-14 17:20:09 -0500

    more updates to the passport profile

--- web-holium-com/src/app/types.ts
+++ web-holium-com/src/app/types.ts
@@ -46,3 +46,36 @@ export type PassportProfile = {
   // signature chain type? .. ask about this
   chain: string;
 };
+
+// crypto/chain/wallet stuff (ask paul)
+export interface PKI_STATE {
+  chain_owner_entities: Array<string>;
+  entity_to_public_keys: Object<string, any>;
+  public_key_to_nonce: Object<string, number>;
+  entity_to_value: Object<string, number>;
+  public_key_to_entity: Object<string, string>;
+}
+
+export interface EPOCH_NODE_POC {
+  link_id: 'EPOCH_NODE_POC' | 'PASSPORT_ROOT';
+  previous_epoch_hash: string;
+  epoch_block_number: number;
+  data_block_number: number;
+  timestamp: number;
+  pki_state: PKI_STATE;
+  transaction_types: {
+    link_names: Array<string>;
+    link_structs: Any;
+  };
+  data_structs: {
+    struct_names: Array<string>;
+    struct_types: Any;
+  };
+  sig_chain_settings: any;
+  // #TODO, might require custom types, or a list of keys
+  // epoch_length : Number
+  // epoch_time_period
+  // whitelist_keys: Array<String>,
+  // blacklist_keys: Array<String>,
+  // signing_key, only required in root node
+}
